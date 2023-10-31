8b510f86b85b998e54d01f25669e57476a01ce4a
tom2tom <tpgww@onepost.net>
2023-10-31 13:44:16 +1100

    implement 'type' parameter processing process $params instead of $_GET directly

--- modules/FilePicker/action.filepicker.php
+++ modules/FilePicker/action.filepicker.php
@@ -17,9 +17,11 @@
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-use FilePicker\TemporaryProfileStorage;
+use CMSMS\FilePickerProfile;
+use CMSMS\FileType;
 use FilePicker\PathAssistant;
 use FilePicker\Profile;
+use FilePicker\TemporaryProfileStorage;
 
 if( !isset($gCms) ) exit;
 if( !check_login(FALSE) ) exit; // admin only.... but any admin
@@ -36,46 +38,93 @@ $clean_str = function($in) {
     return (string)$in;
 };
 
+$sortfiles = function($file1,$file2) {
+    if ($file1['isdir'] && !$file2['isdir']) return -1;
+    if (!$file1['isdir'] && $file2['isdir']) return 1;
+    return strnatcasecmp($file1['name'],$file2['name']);
+};
+
+$get_thumbnail_tag = function($file,$path,$url) {
+    $imagetag = '';
+    $imagepath = $path.DIRECTORY_SEPARATOR.'thumb_'.$file;
+    if( is_file($imagepath) ) {
+        $imageurl = $url.'/thumb_'.$file;
+        $imagetag = '<img src="'.$imageurl.'" alt="'.$file.'" title="'.$file.'">';
+    }
+    return $imagetag;
+};
+
+$accept_file = function(Profile $profile,$assistant,$path,$filename) {
+    if( $filename == '.' ) {
+        return FALSE;
+    }
+    $fullpath = cms_join_path($path,$filename);
+    if( $filename == '..' || $this->is_acceptable_filename($profile,$filename) ) {
+        return $assistant->is_relative($fullpath);
+    }
+    return FALSE;
+};
+
 /*
 In principle, the request-parameters might include
-CMS_SECURE_PARAM_NAME,'mact','showtemplate'
+CMS_SECURE_PARAM_NAME,'mact','showtemplate',
 '_enc','inst','subdir','nosub','sig','useprefix'
 and/or none|some|all profile properties, some of which may override corresponding current property value:
-'id'(RO),'name'(RO),'create_date'(RO),'modified_date'(?),'file_extensions','prefix'
-'top'(RO),'type','can_upload','can_mkdir','can_delete','show_thumbs','show_hidden','sort','match_prefix','exclude_prefix'
+'id'(RO),'name'(RO),'create_date'(RO),'modified_date'(?),'file_extensions','prefix',
+'top'(RO),'type','can_upload','can_mkdir','can_delete','show_thumbs','show_hidden',
+'sort','match_prefix','exclude_prefix'
 */
 //
 // initialization
 //
-$sesskey = md5(__FILE__);
-if( isset($_GET['_enc']) ) {
-    $parms = json_decode(base64_decode($_GET['_enc']),TRUE);
-    unset($_GET['_enc']);
-    if( $params && is_array($parms) ) $_GET = array_merge($_GET,$parms);
+if( isset($params['_enc']) ) {
+    $diropts = json_decode(base64_decode($params['_enc']),TRUE);
+    unset($params['_enc']);
+    if( $diropts && is_array($diropts) ) {
+        $params = array_merge($params,$diropts);
+        $params['useprefix'] = true;
+    }
 }
 
-$inst = get_parameter_value($_GET,'inst');
-$nosub = (int) get_parameter_value($_GET,'nosub');
-$sig = $clean_str(get_parameter_value($_GET,'sig'));
+$inst = isset($params['inst']) ? $clean_str($params['inst']) : '';
+$nosub = isset($params['nosub']) ? cms_to_bool($params['nosub']) : false;
+$sig = isset($params['sig']) ? $clean_str($params['sig']) : '';
 $profile = ( $sig ) ? TemporaryProfileStorage::get($sig) : null; // no object
 if( !$profile ) $profile = $this->get_default_profile();
 if( $profile && !$sig ) { //CHECKME
     $sig = TemporaryProfileStorage::set($profile);
 }
 
-// get our absolute top directory, and its matching url
-$topdir = $profile->top;
+// tailor the profile
+$custom = [];
+if( !$this->CheckPermission('Modify Files') ) { //TODO is this the relevant perm for 'normal' uploading?
+    $custom = [
+     'can_upload'=>FilePickerProfile::FLAG_NONE,
+     'can_delete'=>FilePickerProfile::FLAG_NONE,
+     'can_mkdir'=>FilePickerProfile::FLAG_NONE];
+}
+$type = isset($params['type']) ? $clean_str($params['type']) : FileType::TYPE_ANY;
+$custom['type'] = $type; //profile type may be unchanged
+$profile2 = $profile->overrideWith($custom); //CHECKME replacement object cached anywhere? OK?
+
+// get our absolute top directory
+$topdir = $profile2->top;
 if( !$topdir ) $topdir = $config['uploads_path'];
 $assistant = new PathAssistant($config,$topdir);
 
 // get our current working directory relative to $topdir
-// use cwd stored in session first... then if necessary the profile topdir, then if necessary, the absolute topdir
+// try cwd stored in session first... then if necessary the profile topdir, then if necessary, the absolute topdir
+$sesskey = md5(__FILE__);
 $cwd = '';
-if( isset($_SESSION[$sesskey]) ) $cwd = trim($_SESSION[$sesskey]);
-if( !$cwd && $profile->top ) $cwd = $assistant->to_relative($profile->top);
-if( !$nosub && isset($_GET['subdir']) ) {
+if( isset($_SESSION[$sesskey]) ) {
+    $cwd = trim($_SESSION[$sesskey]);
+}
+if( !$cwd && $profile2->top ) {
+    $cwd = $assistant->to_relative($profile2->top);
+}
+if( !$nosub && isset($params['subdir']) ) {
     try {
-        $cwd .= DIRECTORY_SEPARATOR . cms_html_entity_decode(trim(cleanValue($_GET['subdir'])));
+        $cwd .= DIRECTORY_SEPARATOR . cms_html_entity_decode(trim($params['subdir']));
         $cwd = $assistant->to_relative($assistant->to_absolute($cwd));
     }
     catch( Exception $e ) {
@@ -92,86 +141,62 @@ $_SESSION[$sesskey] = $cwd;
 $starturl = $assistant->relative_path_to_url($cwd);
 $startdir = $assistant->to_absolute($cwd);
 
-$parms = [];
-if( !$this->CheckPermission('Modify Files') ) {
-    $parms = ['can_upload'=>FALSE, 'can_delete'=>FALSE, 'can_mkdir'=>FALSE ];
-}
-$useprefix = cms_to_bool(get_parameter_value($_GET,'useprefix'));
-if( $useprefix ) {
-    $fullname = cms_join_path($startdir,'XAZ');//fake/placeholder hence no realpath() use in Assistant
+if( isset($params['useprefix']) && cms_to_bool($params['useprefix']) ) {
+    $fullname = cms_join_path($startdir,'XAZ');//fake/placeholder hence realpath() in Assistant N/A
     $relpath = substr($fullname, strlen($assistant->get_top_dir()));
     $fullurl = $starturl.'/XAZ';
-    $prefix = substr($fullurl, 0, (strlen($fullurl) - strlen($relpath) + 1));
-//    $parms['prefix'] = $prefix;
+//  $prefix = substr($fullurl, 0, (strlen($fullurl) - strlen($relpath) + 1)); //includes CMS_ROOT_URL
+    $l1 = strlen($fullurl) -strlen($relpath) + 1;
+    $l2 = strlen(CMS_ROOT_URL);
+    $prefix = substr($fullurl, $l2, $l1-$l2);
 } else {
-//    $parms['prefix'] = '';
     $prefix = '';
-}
-if ( $parms ) {
-    $profile = $profile->overrideWith($parms); //CHECKME replacement object automatically cached?
 }
 
 $filemanager = cms_utils::get_module('FileManager');
-
-$sortfiles = function($file1,$file2) {
-    if ($file1["isdir"] && !$file2["isdir"]) return -1;
-    if (!$file1["isdir"] && $file2["isdir"]) return 1;
-    return strnatcasecmp($file1["name"],$file2["name"]);
-};
-
-$accept_file = function(Profile $profile,$cwd,$path,$filename) use ($assistant) {
-    if( $filename == '.' ) return FALSE;
-    $fullpath = cms_join_path($path,$filename);
-    if( $filename == '..' ) {
-        return $assistant->is_relative($fullpath);
-    }
-    if( !$profile->show_hidden && ( startswith($filename,'.') || startswith($filename,'_') ) ) return FALSE;
-    if( !$this->is_acceptable_filename($profile,$filename) ) return FALSE;
-    return $assistant->is_relative($fullpath);
-};
-
-$get_thumbnail_tag = function($file,$path,$url) {
-    $imagetag = '';
-    $imagepath = $path.DIRECTORY_SEPARATOR.'thumb_'.$file;
-    if( is_file($imagepath) ) {
-        $imageurl = $url.'/thumb_'.$file;
-        $imagetag = '<img src="'.$imageurl.'" alt="'.$file.'" title="'.$file.'">';
-    }
-    return $imagetag;
-};
 
 //
 // get our file list
 //
 $files = $thumbs = [];
+$filesizename = [' bytes', ' kb', ' Mb']; //c.f. FileManager equivalents
 $dh = dir($startdir);
 while( FALSE !== ($filename = $dh->read()) ) {
     if( $filename == '.' ) continue;
     if( $filename == '..' && !$cwd ) continue;
     $fullname = cms_join_path($startdir,$filename);
-    $file = array();
-    $file['name'] = $filename;
-    $file['fullpath'] = $fullname;
-    $file['fullurl'] = $starturl.'/'.$filename;
-    $file['isdir'] = is_dir($fullname);
-    if( $file['isdir'] ) {
+    if( is_dir($fullname) ) {
+        $file = [
+        'isdir' => true,
+        'name' => $filename,
+        'ext' => '',
+        'fullpath' => $fullname,
+        'fullurl' => $starturl.'/'.$filename
+        ];
         $file['relurl'] = $file['fullurl'];
         $file['icon'] = $filemanager->GetFileIcon('-',TRUE);
         $file['isparent'] = ($filename == '..');
-    } else {
-        if( !$accept_file($profile, $cwd, $startdir, $filename) ) {
-          continue;
-        }
-        //NOTE relative urls for selected items are pretty much useless
-        //unless a suitable base-url (== func(profile->top) or assistant->_topurl or ? )
-        //is available in context
+        $parms = ['subdir'=>$filename,'inst'=>$inst,'sig'=>$sig];
+        if( $type ) { $parms['type'] = $type; } // pass-thru, not for directory-selection
+        //to avoid lots of urlencode changes, we simply merge all the params
+        $url = $this->create_url($id,'filepicker',$returnid,['_enc'=>base64_encode(json_encode($parms))]).'&showtemplate=false';
+        $file['chdir_url'] = $url;
+        $files[$filename] = $file;
+    } elseif( $accept_file($profile2,$assistant,$startdir,$filename) ) {
+        $file = [
+        'isdir' => false,
+        'name' => $filename,
+        'fullpath' => $fullname,
+        'fullurl' => $starturl.'/'.$filename
+        ];
         $relpath = $assistant->to_relative($fullname);
         $file['relurl'] = strtr($relpath,'\\','/');
-        $file['ext'] = strtolower(substr($filename,strrpos($filename,".")+1));
+        $p = strrpos($filename,'.');
+        $file['ext'] = ($p > 0) ? strtolower(substr($filename,$p+1)) : '';
         $file['icon'] = $filemanager->GetFileIcon('.'.$file['ext'],FALSE);
         $file['filetype'] = $this->_typehelper->get_file_type($fullname);
         $file['isparent'] = FALSE;
-        $file['is_thumb'] = $this->_typehelper->is_thumb($filename);
+        $file['is_thumb'] = $this->_typehelper->is_thumb($fullname);
         $file['dimensions'] = '';
         $file['is_image'] = $this->_typehelper->is_image($fullname);
         if( $file['is_image'] && !$file['is_thumb'] ) {
@@ -180,24 +205,18 @@ while( FALSE !== ($filename = $dh->read()) ) {
             $imgsize = @getimagesize($fullname);
             if( $imgsize ) $file['dimensions'] = $imgsize[0].' x '.$imgsize[1];
         }
+        $info = @stat($fullname);
+        if( $info && $info['size'] > 0) {
+            $i = floor(log($info['size'], 1024));
+            $file['size'] = round(($info['size'] / 1024 ** $i), 2) . $filesizename[$i];
+        } else {
+            $file['size'] = '';
+        }
+        $files[$filename] = $file;
     }
-
-    $info = @stat($fullname);
-    $filesizename = array(" Bytes", " KB", " MB");
-    if( $info && $info['size'] > 0) {
-        $file['size'] = round($info['size']/pow(1024, ($i = floor(log($info['size'], 1024)))), 2) . $filesizename[$i];
-    } else {
-        $file['size'] = '';
-    }
-    if( $file['isdir'] ) {
-        $parms = [ 'subdir'=>$filename, 'inst'=>$inst, 'sig'=>$sig, 'useprefix'=>1 ];
-        $url = $this->create_url($id,'filepicker',$returnid)."&showtemplate=false&_enc=".base64_encode(json_encode($parms));
-        $file['chdir_url'] = $url;
-    }
-    $files[$filename] = $file;
 }
 
-if( $profile->show_thumbs && $thumbs ) {
+if( $profile2->show_thumbs && $thumbs ) {
     // remove from the list thumbnails that are not orphaned
     foreach( $thumbs as $thumb ) {
         if( isset($files[$thumb]) ) unset($files[$thumb]);
@@ -231,7 +250,8 @@ $smarty->assign('sig',$sig);
 $smarty->assign('inst',$inst);
 $smarty->assign('mod',$this);
 $smarty->assign('prefix',$prefix);
-$smarty->assign('profile',$profile);
+$smarty->assign('profile',$profile2);
+$smarty->assign('type',$type);
 $lang = [];
 $lang['confirm_delete'] = $this->Lang('confirm_delete');
 $lang['ok'] = $this->Lang('ok');
