01ab821cf8766c6f79854d3daa91d866d56dbca8
tom2tom <tpgww@onepost.net>
2023-11-10 20:17:51 +1100

    add TODO comment

--- modules/FileManager/action.fileaction.php
+++ modules/FileManager/action.fileaction.php
@@ -16,6 +16,9 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+// TODO handle $params['newsort'] in redirect
+// TODO handle path & url separators robustly
+
 if (!function_exists("cmsms")) exit;
 if (!$this->CheckPermission("Modify Files") && !$this->AdvancedAccessAllowed()) exit;
 if (!isset($params["path"])) $this->Redirect($id, 'defaultadmin');
