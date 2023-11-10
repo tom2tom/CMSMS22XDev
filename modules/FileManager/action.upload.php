8336a71f40d04716a79ee95d357fec8aafc7a4e3
tom2tom <tpgww@onepost.net>
2023-11-10 20:17:05 +1100

    use FileTypeHelper to check acceptability

--- modules/FileManager/action.upload.php
+++ modules/FileManager/action.upload.php
@@ -16,13 +16,18 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+use CMSMS\FileTypeHelper;
+use CMSMS\HookManager;
+
 if (!function_exists("cmsms")) exit;
 if (!$this->CheckPermission("Modify Files") && !$this->AdvancedAccessAllowed()) exit;
 require_once( __DIR__.'/lib/class.jquery_upload_handler.php' );
 
 class FileManagerUploadHandler extends jquery_upload_handler
 {
-  function __construct($options=[])
+  private $helper;
+
+  function __construct($options=array())
   {
     if( !is_array($options) ) $options = array();
 
@@ -30,43 +35,43 @@ class FileManagerUploadHandler extends jquery_upload_handler
     $options['orient_image'] = false;  // turn off auto image rotation
     $options['image_versions'] = array();
 
-    $options['upload_dir'] = filemanager_utils::get_full_cwd().'/';
+    $options['upload_dir'] = filemanager_utils::get_full_cwd().DIRECTORY_SEPARATOR;
     $options['upload_url'] = filemanager_utils::get_cwd_url().'/';
 
     // set everything up.
     parent::__construct($options);
   }
 
-  protected function is_file_acceptable( $file )
+  protected function is_file_acceptable($file)
   {
-      $config = \cms_config::get_instance();
-      if( !$config['developer_mode'] ) {
-          $ext = strtolower(substr(strrchr($file, '.'), 1));
-          if( startswith($ext,'php') || endswith($ext,'php') ) return FALSE;
-      }
-      return TRUE;
+    $config = cms_config::get_instance();
+    if( $config['developer_mode'] ) return TRUE;
+    if( !isset($this->helper) ) {
+      $this->helper = new FileTypeHelper($config);
+    }
+    return !$this->helper->is_executable($file);
   }
 
   protected function after_uploaded_file($fileobject)
   {
-      // here we may do image handling, and other cruft.
-      if( is_object($fileobject) && $fileobject->name != '' ) {
+    // here we may do image handling, and other cruft.
+    if( is_object($fileobject) && $fileobject->name != '' ) {
 
-          $mod = cms_utils::get_module('FileManager');
-          $file = filemanager_utils::join_path(filemanager_utils::get_full_cwd(),$fileobject->name);
-          $parms['file'] = $file;
-          $parms = \CMSMS\HookManager::do_hook( 'FileManager::OnFileUploaded', $parms );
-          if( is_array($parms) && isset($parms['file']) ) $file = $parms['file']; // file name could have changed.
+      $mod = cms_utils::get_module('FileManager');
+      $file = filemanager_utils::join_path(filemanager_utils::get_full_cwd(),$fileobject->name);
+      $parms['file'] = $file;
+      $parms = HookManager::do_hook( 'FileManager::OnFileUploaded', $parms );
+      if( is_array($parms) && isset($parms['file']) ) $file = $parms['file']; // file name could have changed.
 
-          $thumb = FALSE;
-          if( $mod->GetPreference('create_thumbnails') ) {
-              $thumb = filemanager_utils::create_thumbnail($file, NULL, TRUE);
-          }
-
-          $str = basename($file).' uploaded to '.filemanager_utils::get_full_cwd();
-          if( $thumb ) $str .= ' and a thumbnail was generated';
-          audit('',$mod->GetName(),$str);
+      $thumb = FALSE;
+      if( $mod->GetPreference('create_thumbnails') ) {
+          $thumb = filemanager_utils::create_thumbnail($file, NULL, TRUE);
       }
+
+      $str = basename($file).' uploaded to '.filemanager_utils::get_full_cwd();
+      if( $thumb ) $str .= ' and a thumbnail was generated';
+      audit('',$mod->GetName(),$str);
+    }
   }
 }
 
@@ -82,20 +87,20 @@ header('Access-Control-Allow-Methods: OPTIONS, HEAD, GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: X-File-Name, X-File-Type, X-File-Size');
 
 switch ($_SERVER['REQUEST_METHOD']) {
-    case 'OPTIONS':
-        break;
-    case 'HEAD':
-    case 'GET':
-        $upload_handler->get();
-        break;
-    case 'POST':
-        $upload_handler->post();
-        break;
-    case 'DELETE':
-        $upload_handler->delete();
-        break;
-    default:
-        header('HTTP/1.1 405 Method Not Allowed');
+  case 'OPTIONS':
+    break;
+  case 'HEAD':
+  case 'GET':
+    $upload_handler->get();
+    break;
+  case 'POST':
+    $upload_handler->post();
+    break;
+  case 'DELETE':
+    $upload_handler->delete();
+    break;
+  default:
+    header('HTTP/1.1 405 Method Not Allowed');
 }
 
 exit;
