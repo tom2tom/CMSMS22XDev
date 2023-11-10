a76c70c8e76200de36d31615fc2cc7f8c260f924
tom2tom <tpgww@onepost.net>
2023-11-10 19:43:39 +1100

    tailor filepicker title tolerate longform and shortform urls provided by filepicker

--- modules/MicroTiny/lib/js/tinymce/plugins/cmsms_filepicker/plugin.js
+++ modules/MicroTiny/lib/js/tinymce/plugins/cmsms_filepicker/plugin.js
@@ -20,11 +20,11 @@ tinymce.PluginManager.add('cmsms_filepicker', function(editor) {
 
         // generate a uniquie id for the active editor so we can access it later.
         var inst = 'i'+(new Date().getTime()).toString(16);
-        tinymce.activeEditor.dom.setAttrib(tinyMCE.activeEditor.dom.select('html'),'data-cmsfp-instance',inst);
+        tinymce.activeEditor.dom.setAttrib(tinymce.activeEditor.dom.select('html'),'data-cmsfp-instance',inst);
 
         if( !top.document.CMSFileBrowser ) top.document.CMSFileBrowser = {type:meta.filetype};
         top.document.CMSFileBrowser.onselect = function(inst,file) {
-            file = cms_data.root_url + file;
+
             function basename(str) {
                 var sw, pp, base,
                  last = str.charAt(str.length - 1);
@@ -41,6 +41,11 @@ tinymce.PluginManager.add('cmsms_filepicker', function(editor) {
                 return base;
             }
 
+            if (file.charAt(0) === '/') {
+                file = cms_data.root_url + file; //longform picker result
+            } else {
+                file = cms_data.uploads_url + '/' + file; //shortform picker result
+            }
             var opts = {};
             if( meta.filetype === 'file' ) {
                 opts.text = basename(file);
@@ -51,28 +56,28 @@ tinymce.PluginManager.add('cmsms_filepicker', function(editor) {
             top.document.CMSFileBrowser.onselect = null;
             mywin.close();
         };
-
-
-        // here we open the filepicker window.
-        var key = meta.filetype === 'image' ? 'imagebrowser_title' : 'filebrowser_title';
-        var url = cmsms_tiny.filepicker_url + '&m1_inst=' + encodeURIComponent(inst) + '&m1_type='+meta.filetype + '&m1_useprefix=1';
-        mywin = tinymce.activeEditor.windowManager.open({
-            title : cmsms_tiny[key],
-            file : url,
-            classes : 'filepicker',
-            height : height,
-            width : width,
-            inline : 1,
-            resizable : true,
-            maximizable : true,
-        }, {
-            onFileSelected: function(filename) {
-                console.debug('woot got callback with '+filename);
-            }
-        // TODO if meta.filetype === 'image' trigger a click on the images-filter element
-        //iframe find <span class="js-trigger filepicker-button" data-fb-type="image"</span>
-        //OR if needed for all types: iframe find <span class="js-trigger filepicker-button" data-fb-type="reset"</span>
-        });
+        //TODO top.document.CMSFileBrowser.onopen = function() { activate relevant picker-filter button }
+        if (typeof cmsms_tiny.filepicker_url !== 'undefined' && cmsms_tiny.filepicker_url) {
+            // here we open the filepicker window. TODO relevance of former trailing '&m1_field=' parameter in filepicker_url?
+            var url = cmsms_tiny.filepicker_url + '&m1_inst=' + encodeURIComponent(inst) + '&m1_type='+meta.filetype + '&m1_useprefix=1';
+            var key = meta.filetype === 'image' ? 'imagebrowser_title' : 'filebrowser_title';
+            mywin = tinymce.activeEditor.windowManager.open({
+                title : cmsms_tiny[key],
+                file : url,
+                classes : 'filepicker',
+                height : height,
+                width : width,
+                inline : 1,
+                resizable : true,
+                maximizable : true
+/*            },//TODO invalid API for .open method
+              {
+                onFileSelected: function(filename) {
+                    console.debug('woot got callback with '+filename);
+              }
+*/
+            });
+        }
     }
     return false;
 });
