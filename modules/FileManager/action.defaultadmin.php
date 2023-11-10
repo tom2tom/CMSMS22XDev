8c53b7f6319c27307e3990576e2135b6fb6e6a0f
tom2tom <tpgww@onepost.net>
2023-11-10 20:21:12 +1100

    cleanup

--- modules/FileManager/action.defaultadmin.php
+++ modules/FileManager/action.defaultadmin.php
@@ -16,45 +16,43 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-if (!function_exists("cmsms")) exit;
+if (!function_exists('cmsms')) exit;
 if (!$this->CheckPermission('Modify Files')) exit;
 
-if (!empty($params["fmmessage"])) {
-    // gotta get rid of this stuff.
-    $count="";
-    if (!empty($params["fmmessagecount"])) $count = $params["fmmessagecount"];
-    echo $this->ShowMessage($this->Lang($params["fmmessage"],$count));
-}
-
-if (!empty($params["fmerror"])) {
+if (!empty($params['fmmessage'])) {
     // gotta get rid of this stuff
-    $count="";
-    if (!empty($params["fmerrorcount"])) $count = $params["fmerrorcount"];
-    echo $this->ShowErrors($this->Lang($params["fmerror"],$count));
+    $count = (!empty($params['fmmessagecount'])) ? $params['fmmessagecount'] : '';
+    echo $this->ShowMessage($this->Lang($params['fmmessage'],$count));
 }
 
-if (isset($params["newsort"])) $this->SetPreference("sortby",$params["newsort"]);
-
-$path = trim(ltrim(filemanager_utils::get_cwd(),'/'));
-if( filemanager_utils::can_do_advanced() && $this->GetPreference('advancedmode',0) ) {
-    $path = '::top::/'.$path;
+if (!empty($params['fmerror'])) {
+    // gotta get rid of this stuff
+    $count = (!empty($params['fmerrorcount'])) ? $params['fmerrorcount'] : '';
+    echo $this->ShowErrors($this->Lang($params['fmerror'],$count));
 }
-$tmp_path_parts = explode('/',$path);
+
+if (isset($params['newsort'])) {
+   $_SESSION['FMnewsortby'] = trim(cleanValue($params['newsort']));
+}
+
+$path = trim(ltrim(filemanager_utils::get_cwd(),DIRECTORY_SEPARATOR));
+if (filemanager_utils::can_do_advanced() && $this->GetPreference('advancedmode',0)) {
+    $path = '::top::'.DIRECTORY_SEPARATOR.$path; // placeholder for 'root'
+}
+$tmp_path_parts = explode(DIRECTORY_SEPARATOR,$path);
 $path_parts = [];
-for( $i = 0; $i < count($tmp_path_parts); $i++ ) {
+for ($i = 0, $n = count($tmp_path_parts); $i < $n; $i++) {
+    if (!$tmp_path_parts[$i]) continue;
     $obj = new stdClass();
-    if( !$tmp_path_parts[$i] ) continue;
     $obj->name = $tmp_path_parts[$i];
-    if( $obj->name == '::top::' ) {
+    if ($obj->name == '::top::') {
         $obj->name = 'root';
     }
-    if( $i < count($tmp_path_parts) - 1 ) {
-        // not the last entry
-        $fullpath = implode('/',array_slice($tmp_path_parts,0,$i+1));
-        if( startswith($fullpath,'::top::') ) $fullpath = substr($fullpath,7);
-        $obj->url = $this->create_url( $id, 'changedir', '',[ 'setdir' => $fullpath ] );
-    } else {
-        // the last entry... no link
+    if ($i < $n - 1) {
+        // not the last/lowest path-part
+        $fullpath = implode(DIRECTORY_SEPARATOR,array_slice($tmp_path_parts,0,$i+1));
+        if (startswith($fullpath,'::top::')) $fullpath = substr($fullpath,7); // no placeholder
+        $obj->url = $this->create_url($id,'changedir','',['setdir' => $fullpath]); // TODO '&amp;' seps ok?
     }
     $path_parts[] = $obj;
 }
@@ -62,5 +60,5 @@ $smarty->assign('path',$path);
 $smarty->assign('path_parts',$path_parts);
 echo $this->ProcessTemplate('fmpath.tpl');
 
-include(__DIR__."/uploadview.php");
-include(__DIR__."/action.admin_fileview.php"); // this is also a standalone action
+include __DIR__.DIRECTORY_SEPARATOR.'uploadview.php';
+include __DIR__.DIRECTORY_SEPARATOR.'action.admin_fileview.php'; // this is also a standalone action
