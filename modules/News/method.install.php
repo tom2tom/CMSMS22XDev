9cb9d6f44606dd8eee94944c36921a0b0d921820
tom2tom <tpgww@onepost.net>
2023-11-20 18:38:19 +1100

    consistent font families

--- modules/News/method.install.php
+++ modules/News/method.install.php
@@ -293,7 +293,7 @@ $db->Execute($query, array($catid, 'General', -1));
 # Setup initial news article
 $articleid = $db->GenID(CMS_DB_PREFIX."module_news_seq");
 $query = 'INSERT INTO '.CMS_DB_PREFIX.'module_news ( NEWS_ID, NEWS_CATEGORY_ID, AUTHOR_ID, NEWS_TITLE, NEWS_DATA, NEWS_DATE, SUMMARY, START_TIME, END_TIME, STATUS, ICON, SEARCHABLE, CREATE_DATE, MODIFIED_DATE ) VALUES (?,?,?,?,?,'.$db->DBTimeStamp(time()).',?,?,?,?,?,?,'.$db->DBTimeStamp(time()).','.$db->DBTimeStamp(time()).')';
-$db->Execute($query, array($articleid, $catid, 1, 'News Module Installed', 'The news module was installed.  Exciting. This news article is not using the Summary field and therefore there is no link to read more. But you can click on the news heading to read only this article.', null, null, null, 'published', null, 1));
+$db->Execute($query, array($articleid, $catid, 1, 'News Module Installed', 'The news module was installed.  Exciting. This news article does not use a Summary field and therefore there is no link to read more. But you can click on the news heading to read the whole article.', null, null, null, 'published', null, 1));
 news_admin_ops::UpdateHierarchyPositions();
 
 # Setup permissions
