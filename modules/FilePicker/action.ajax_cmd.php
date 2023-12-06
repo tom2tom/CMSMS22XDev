a388a2b60f71c32d0d761dd06161bdf9991a6baf
tom2tom <tpgww@onepost.net>
2023-12-07 08:01:31 +1100

    namespace cleanup

--- modules/FilePicker/action.ajax_cmd.php
+++ modules/FilePicker/action.ajax_cmd.php
@@ -1,12 +1,13 @@
 <?php
-use FilePicker\TemporaryProfileStorage;
+
 use FilePicker\PathAssistant;
+use FilePicker\TemporaryProfileStorage;
 use FilePicker\UploadHandler;
 
 if( !isset($gCms) ) exit;
 try {
     if( strtolower($_SERVER['REQUEST_METHOD']) != 'post' ) {
-        throw new \RuntimeException('Invalid request method');
+        throw new RuntimeException('Invalid request method');
     }
 
     $sig = cleanValue(get_parameter_value($_POST,'sig'));
@@ -25,34 +26,34 @@ try {
     $assistant = new PathAssistant($config,$topdir);
 
     $fullpath = $assistant->to_absolute($cwd);
-    if( ! $assistant->is_relative($fullpath) ) throw new \RuntimeException('Invalid cwd '.$cwd);
+    if( ! $assistant->is_relative($fullpath) ) throw new RuntimeException('Invalid cwd '.$cwd);
     if( !isset($this->macos) ) {
         $tmp = php_uname('s');
-        $this->winos = stripos($tmp, 'windo') !== false;// running on some flavour of Windows
-        $this->macos = !$this->winos && stripos($tmp, 'darwin') !== false;// running on some flavour of MacOS
+        $this->winos = stripos($tmp,'windo') !== false;// running on some flavour of Windows
+        $this->macos = !$this->winos && stripos($tmp,'darwin') !== false;// running on some flavour of MacOS
     }
     switch( $cmd ) {
     case 'mkdir':
-        if( !$profile->can_mkdir ) throw new \LogicException('Internal error: mkdir command executed, but profile says we cannot do this');
+        if( !$profile->can_mkdir ) throw new LogicException('Internal error: mkdir command executed, but profile prohibits that operation');
         // TODO hidden-attribute check on Windows
-        if( ($val[0] == '.' && !$this->winos) || ($val[0] == '_' && $this->macos) || ($val[0] == '~' && $this->winos) ) throw new \RuntimeException($this->Lang('error_ajax_invalidfilename'));
-        if( !is_writable($fullpath) ) throw new \RuntimeException($this->Lang('error_ajax_writepermission'));
+        if( ($val[0] == '.' && !$this->winos) || ($val[0] == '_' && $this->macos) || ($val[0] == '~' && $this->winos) ) throw new RuntimeException($this->Lang('error_ajax_invalidfilename'));
+        if( !is_writable($fullpath) ) throw new RuntimeException($this->Lang('error_ajax_writepermission'));
         $destpath = $fullpath.DIRECTORY_SEPARATOR.$val;
-        if( is_dir($destpath) || is_file($destpath) ) throw new \RuntimeException($this->Lang('error_ajax_fileexists'));
-        if( !@mkdir($destpath) ) throw new \RuntimeException($this->Lang('error_ajax_mkdir ',$cwd.DIRECTORY_SEPARATOR.$val)); // TODO $destpath ?
+        if( is_dir($destpath) || is_file($destpath) ) throw new RuntimeException($this->Lang('error_ajax_fileexists'));
+        if( !@mkdir($destpath) ) throw new RuntimeException($this->Lang('error_ajax_mkdir ',$cwd.DIRECTORY_SEPARATOR.$val)); // TODO $destpath ?
         break;
 
     case 'del':
-        if( !$profile->can_delete ) throw new \LogicException('Internal error: del command executed, but profile says we cannot do this');
+        if( !$profile->can_delete ) throw new LogicException('Internal error: del command executed, but profile prohibits that operation');
         $val = basename($val);
-        if( ($val[0] == '.' && !$this->winos) || ($val[0] == '_' && $this->macos) || ($val[0] == '~' && $this->winos) ) throw new \RuntimeException($this->Lang('error_ajax_invalidfilename'));
+        if( ($val[0] == '.' && !$this->winos) || ($val[0] == '_' && $this->macos) || ($val[0] == '~' && $this->winos) ) throw new RuntimeException($this->Lang('error_ajax_invalidfilename'));
         //if( !is_writable($fullpath) ) throw new \RuntimeException($this->Lang('error_ajax_writepermission'));
         $destpath = $fullpath.DIRECTORY_SEPARATOR.$val;
-        if( !is_writable($destpath) ) throw new \RuntimeException($this->Lang('error_ajax_writepermission').' '.$destpath);
+        if( !is_writable($destpath) ) throw new RuntimeException($this->Lang('error_ajax_writepermission').' '.$destpath);
         if( is_dir($destpath) ) {
             // check if the directory is empty
-            if( count(scandir($destpath)) > 2 ) throw new \RuntimeException($this->Lang('error_ajax_dirnotempty'));
-            if( @rmdir($destpath) ) { audit('', 'FilePicker', 'Removed directory: '.$destpath); }
+            if( count(scandir($destpath)) > 2 ) throw new RuntimeException($this->Lang('error_ajax_dirnotempty'));
+            if( @rmdir($destpath) ) { audit('','FilePicker','Removed directory: '.$destpath); }
         } else {
             if( $this->is_image( $destpath ) ) {
                 $thumbnail = $fullpath.DIRECTORY_SEPARATOR.'thumb_'.$val;
@@ -60,14 +61,14 @@ try {
                     @unlink($thumbnail);
                 }
             }
-            if( @unlink($destpath) ) { audit('', 'FilePicker', 'Removed file: '.$destpath); }
+            if( @unlink($destpath) ) { audit('','FilePicker','Removed file: '.$destpath); }
         }
         break;
 
     case 'upload':
-        if( !$profile->can_upload ) throw new \LogicException('Internal error: upload command executed, but profile says you cannot upload');
+        if( !$profile->can_upload ) throw new LogicException('Internal error: upload command executed, but profile prohibits that operation');
         // todo: checks for upload functionality
-        $upload_handler = new UploadHandler($this, $profile, $fullpath);
+        $upload_handler = new UploadHandler($this,$profile,$fullpath);
 
         header('Pragma: no-cache');
         header('Cache-Control: private, no-cache');
@@ -96,10 +97,10 @@ try {
         exit; // exit from here.
 
     default:
-        throw new \RuntimeException('Invalid cmd '.$cmd);
+        throw new RuntimeException('Invalid cmd '.$cmd);
     }
 }
-catch( \Exception $e ) {
+catch( Exception $e ) {
     // throw a 500 error
     debug_to_log('Exception: '.$e->GetMessage());
     debug_to_log($e->GetTraceAsString());
