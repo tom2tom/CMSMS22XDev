77ecc508f3e7a1205da99b7d829afb0d085c7229
tom2tom <tpgww@onepost.net>
2024-03-18 08:35:15 +1100

    also remove uploads folder

--- modules/News/method.uninstall.php
+++ modules/News/method.uninstall.php
@@ -1,25 +1,22 @@
 <?php
 if (!isset($gCms)) exit;
 
-$this->DeleteTemplate('displaysummary');
-$this->DeleteTemplate('displaydetail');
-
 $dict = NewDataDictionary( $db );
 
-$sqlarray = $dict->DropTableSQL( CMS_DB_PREFIX."module_news" );
+$sqlarray = $dict->DropTableSQL( CMS_DB_PREFIX.'module_news' );
 $dict->ExecuteSQLArray($sqlarray);
 
-$sqlarray = $dict->DropTableSQL( CMS_DB_PREFIX."module_news_categories" );
+$sqlarray = $dict->DropTableSQL( CMS_DB_PREFIX.'module_news_categories' );
 $dict->ExecuteSQLArray($sqlarray);
 
-$sqlarray = $dict->DropTableSQL( CMS_DB_PREFIX."module_news_fielddefs" );
+$sqlarray = $dict->DropTableSQL( CMS_DB_PREFIX.'module_news_fielddefs' );
 $dict->ExecuteSQLArray($sqlarray);
 
-$sqlarray = $dict->DropTableSQL( CMS_DB_PREFIX."module_news_fieldvals" );
+$sqlarray = $dict->DropTableSQL( CMS_DB_PREFIX.'module_news_fieldvals' );
 $dict->ExecuteSQLArray($sqlarray);
 
-$db->DropSequence( CMS_DB_PREFIX."module_news_seq" );
-$db->DropSequence( CMS_DB_PREFIX."module_news_categories_seq" );
+$db->DropSequence( CMS_DB_PREFIX.'module_news_seq' );
+$db->DropSequence( CMS_DB_PREFIX.'module_news_categories_seq' );
 
 $this->RemovePermission('Modify News');
 $this->RemovePermission('Approve News');
@@ -28,10 +25,7 @@ $this->RemovePermission('Delete News');
 // Remove all preferences for this module
 $this->RemovePreference();
 
-// And all Templates
-$this->DeleteTemplate();
-
-#Setup events
+// And events
 $this->RemoveEvent('NewsArticleAdded');
 $this->RemoveEvent('NewsArticleEdited');
 $this->RemoveEvent('NewsArticleDeleted');
@@ -41,19 +35,22 @@ $this->RemoveEvent('NewsCategoryDeleted');
 
 $this->RemoveSmartyPlugin();
 
-cms_route_manager::del_static('',$this->GetName());
+$me = $this->GetName();
+cms_route_manager::del_static('',$me);
 
-// remove templates
-// and template types.
+// Remove all module-templates
+$this->DeleteTemplate();
+
+// And DM templates and template types
 try {
-  $types = CmsLayoutTemplateType::load_all_by_originator($this->GetName());
+  $types = CmsLayoutTemplateType::load_all_by_originator($me);
   if( is_array($types) && count($types) ) {
     foreach( $types as $type ) {
       $templates = $type->get_template_list();
-      if( is_array($templates) && count($templates) ) {
-	foreach( $templates as $template ) {
-	  $template->delete();
-	}
+      if( $templates && is_array($templates) ) {
+        foreach( $templates as $template ) {
+          $template->delete();
+        }
       }
       $type->delete();
     }
@@ -61,6 +58,13 @@ try {
 }
 catch( Exception $e ) {
   // log it
-  audit('',$this->GetName(),'Uninstallation error: '.$e->GetMessage());
+  audit('',$me,'Uninstallation error: '.$e->GetMessage());
 }
+
+// Remove any uploads
+$fp = cms_join_path($config['uploads_path'],'news');
+if( is_dir($fp) ) {
+  recursive_delete($fp);
+}
+
 ?>
