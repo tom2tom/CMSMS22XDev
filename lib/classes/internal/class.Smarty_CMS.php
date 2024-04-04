18fc440372651f00a22d43ebcebbab3bf1acc764
tom2tom <tpgww@onepost.net>
2024-04-05 09:11:17 +1100

    add missing attributes

--- lib/classes/internal/class.Smarty_CMS.php
+++ lib/classes/internal/class.Smarty_CMS.php
@@ -566,13 +566,16 @@ class Smarty_TemplateCaller
         self::$static_classes = &$smarty->security_policy->static_classes; //TODO check with Smarty5
     }
 
-    public function __call($name, $args) {
+    #[\ReturnTypeWillChange]
+    public function __call($name, $args)
+    {
         if ($this->php_functions !== null && (!$this->php_functions || in_array($name, $this->php_functions))) {
             return $name(...$args);
         }
         return "<!-- prohibited function $name called -->";
     }
 
+    #[\ReturnTypeWillChange]
     public static function __callStatic($name, $args)
     {
         if (self::$static_classes !== null && (!self::$static_classes ||
