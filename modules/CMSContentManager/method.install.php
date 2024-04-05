31ea1bf48f10ce84cde5fcaad134f27af8299308
tom2tom <tpgww@onepost.net>
2024-04-06 10:19:44 +1100

    change default tab-order preferences

--- modules/CMSContentManager/method.install.php
+++ modules/CMSContentManager/method.install.php
@@ -33,11 +33,11 @@ if( !isset($gCms) ) exit;
 $this->SetPreference('locktimeout',60);
 $this->SetPreference('lockrefresh',120);
 // default page-edit tabs display order
-$this->SetPreference('order_TAB_MAIN',10);
-$this->SetPreference('order_TAB_NAV',15);
-$this->SetPreference('order_TAB_LOGIC',20);
-$this->SetPreference('order_TAB_OPTIONS',25);
-$this->SetPreference('order_TAB_PERMS',30);
+$this->SetPreference('order_TAB_MAIN',2);
+$this->SetPreference('order_TAB_NAV',4);
+$this->SetPreference('order_TAB_LOGIC',6);
+$this->SetPreference('order_TAB_OPTIONS',8);
+$this->SetPreference('order_TAB_PERMS',10);
 
 #
 # EOF
--- modules/CMSContentManager/method.upgrade.php
+++ modules/CMSContentManager/method.upgrade.php
@@ -3,9 +3,9 @@ if( !isset($gCms) ) exit;
 
 if( version_compare($oldversion,'1.1.12') < 0 ) {
 	// default page-edit tabs display order
-	$this->SetPreference('order_TAB_MAIN', 10);
-	$this->SetPreference('order_TAB_NAV', 15);
-	$this->SetPreference('order_TAB_LOGIC', 20);
-	$this->SetPreference('order_TAB_OPTIONS', 25);
-	$this->SetPreference('order_TAB_PERMS', 30);
+	$this->SetPreference('order_TAB_MAIN', 2);
+	$this->SetPreference('order_TAB_NAV', 4);
+	$this->SetPreference('order_TAB_LOGIC', 6);
+	$this->SetPreference('order_TAB_OPTIONS', 8);
+	$this->SetPreference('order_TAB_PERMS', 10);
 }
