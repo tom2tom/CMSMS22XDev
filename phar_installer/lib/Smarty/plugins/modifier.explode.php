682cd723325f6b33cb849459c744f9791173b120
tom2tom <tpgww@onepost.net>
2024-08-28 08:07:51 +1000

    revert typo

--- phar_installer/lib/Smarty/plugins/modifier.explode.php
+++ phar_installer/lib/Smarty/plugins/modifier.explode.php
@@ -20,6 +20,6 @@
  */
 function smarty_modifier_explode($separator, ?string $string, ?int $limit = null)
 {
-    // provide $string default to prevent deprecation error in PHP >=8.1
+    // provide $string default to prevent deprecation errors in PHP >=8.1
     return explode($separator, $string ?? '', $limit ?? PHP_INT_MAX);
 }
