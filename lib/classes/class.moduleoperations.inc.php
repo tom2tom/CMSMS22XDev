1ed94dd68ddf194b177078faca7ac9f90ebc5d0f
tom2tom <tpgww@onepost.net>
2025-02-20 10:41:05 +1100

    revise comment

--- lib/classes/class.moduleoperations.inc.php
+++ lib/classes/class.moduleoperations.inc.php
@@ -590,7 +590,7 @@ final class ModuleOperations
             if( is_array($deps) && count($deps) ) {
                 foreach( $deps as $name => $ver ) {
                     if( $name == $module_name ) continue; // a module cannot depend on itself.
-                    // this is the start of a recursive routine.
+                    // this is the start of an indirect recursive process.
                     // get_module_instance may call _load_module.
                     $obj2 = $this->get_module_instance($name,$ver);
                     if( !is_object($obj2) ) {
