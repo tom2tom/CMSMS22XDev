b3db92ceb55855ccea7857fca77abd3d4fad12d0
tom2tom <tpgww@onepost.net>
2025-03-09 13:45:26 +1100

    code cleanup

--- lib/module.functions.php
+++ lib/module.functions.php
@@ -24,65 +24,66 @@
  *
  * @internal
  * @access private
- * @param array A hash of parameters
- * @param object The smarty template object
+ * @param array $params A hash of parameters
+ * @param object $smarty The smarty template object
  * @return string The module output
  */
 function cms_module_plugin($params,$smarty)
 {
     if( get_class($smarty) == 'Smarty_Parser' ) return ''; // (deprecated Smarty_Parser triggers abort, should be UNUSED) if we are in the parser, we don't process module calls.
 
-    $mid_cache = cms_utils::get_app_data('mid_cache');
-    if( empty($mid_cache) ) $mid_cache = array();
-    for( $i = 0; $i < 10; $i++ ) {
-        $tmp = $i;
-        foreach($params as $key=>$value) {
-            $tmp .= $key.'='.$value;
-        }
-        $id = 'm'.substr(md5($tmp),0,5);
-        if( !isset($mid_cache[$id]) ) {
-            $mid_cache[$id] = $id;
-            break;
-        }
-    }
-    cms_utils::set_app_data('mid_cache',$mid_cache);
-
-    $returnid = CmsApp::get_instance()->get_content_id();
-
-    $modulename = '';
-    $action = 'default';
-    $inline = false;
-    $checkid = '';
-
-    if (isset($params['module'])) {
+    if( isset($params['module']) ) {
         $modulename = $params['module'];
     }
     else {
         return '<!-- ERROR: module name not specified -->';
     }
 
-    if (isset($params['idprefix'])) $id = trim($params['idprefix']);
-    if (isset($params['action']) && $params['action'] != '') {
+    $mid_cache = cms_utils::get_app_data('mid_cache');
+    if( empty($mid_cache) ) $mid_cache = array();
+    for( $i = 0; $i < 10; $i++ ) {
+        $tmp = $i;
+        foreach( $params as $key=>$value ) {
+            $tmp .= $key.'='.$value;
+        }
+        $id = 'm'.substr(md5($tmp), 0, 5);
+        if( !isset($mid_cache[$id]) ) {
+            $mid_cache[$id] = $id;
+            break;
+        }
+    }
+    cms_utils::set_app_data('mid_cache', $mid_cache);
+
+    $returnid = CmsApp::get_instance()->get_content_id();
+
+    if( isset($params['idprefix']) ) {
+        $id = trim($params['idprefix']);
+    }
+    if( !empty($params['action']) ) {
         // action was set in the module tag
         $action = $params['action'];
     }
+    else {
+        $action = 'default';
+    }
 
-    if (isset($_REQUEST['mact'])) {
+    if( isset($_REQUEST['mact']) ) { //OR $params[] ?
         // we're handling an action
         $ary = explode(',', cms_htmlentities($_REQUEST['mact']), 4);
-        $mactmodulename = (isset($ary[0])?$ary[0]:'');
-        if (!strcasecmp($mactmodulename,$params['module'])) {
-            $checkid = (isset($ary[1])?$ary[1]:'');
-            $mactaction = (isset($ary[2])?$ary[2]:'');
+        $mactmodulename = (isset($ary[0]))?$ary[0]:'';
+        if( strcasecmp($mactmodulename, $modulename) == 0 ) {
+            $checkid = (isset($ary[1]))?$ary[1]:'';
+            $mactaction = (isset($ary[2]))?$ary[2]:'';
         }
-        $mactinline = (isset($ary[3]) && $ary[3] == 1?true:false);
+        else {
+            $checkid = '';
+        }
 
-        if ($checkid == $id) {
+        if( $checkid == $id ) {
             // the action is for this instance of the module
-            $inline = $mactinline;
-            if( $inline == true ) {
+            if( isset($ary[3]) && $ary[3] == 1 ) {
                 // and we're inline (the results are supposed to replace
-                // the tag, not {content}
+                // the tag, not {content})
                 $action = $mactaction;
                 $params = array_merge($params, ModuleOperations::get_instance()->GetModuleParameters($id));
             }
@@ -94,16 +95,16 @@ function cms_module_plugin($params,$smarty)
     class_exists($modulename);
     $module = cms_utils::get_module($modulename);
     global $CMS_ADMIN_PAGE, $CMS_LOGIN_PAGE, $CMS_INSTALL;
-    if( $module && ($module->isPluginModule() || (isset($CMS_ADMIN_PAGE) && !isset($CMS_INSTALL) && !isset($CMS_LOGIN_PAGE) ) ) ) {
+    if( $module && ($module->isPluginModule() || (isset($CMS_ADMIN_PAGE) && !isset($CMS_INSTALL) && !isset($CMS_LOGIN_PAGE))) ) {
         @ob_start();
-        $result = $module->DoActionBase($action, $id, $params, $returnid,$smarty);
-        if ($result !== FALSE) echo $result;
+        $result = $module->DoActionBase($action, $id, $params, $returnid, $smarty);
+        if( $result !== FALSE ) echo $result;
         $modresult = @ob_get_contents();
         @ob_end_clean();
 
         if( isset($params['assign']) ) {
-            $smarty->assign(trim($params['assign']),$modresult);
-            return;
+            $smarty->assign(trim($params['assign']), $modresult);
+            return '';
         }
         return $modresult;
 
