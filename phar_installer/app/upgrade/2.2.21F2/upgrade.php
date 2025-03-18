f3e0d3ea7147aa7582eb906aa093ac3cc6999d29
tom2tom <tpgww@onepost.net>
2025-03-18 18:45:35 +1100

    add missing file

---
+++ phar_installer/app/upgrade/2.2.21F2/upgrade.php
@@ -0,0 +1,24 @@
+<?php
+
+$config = CmsApp::get_instance()->GetConfig();
+$dir = cms_join_path($config['admin_path'],'configs');
+if( !is_dir($dir) ) {
+    @mkdir($dir,0777,true);//OR 0775 or 0770
+    touch($dir.DIRECTORY_SEPARATOR.'index.html');
+}
+
+$dir = cms_join_path($config['assets_path'],'configs');
+if( !is_dir($dir) ) {
+    @mkdir($dir,0777,true);//OR 0775 or 0770
+    touch($dir.DIRECTORY_SEPARATOR.'index.html');
+}
+
+$dir = cms_join_path($config['root_path'],'tmp');
+$fp = $dir.DIRECTORY_SEPARATOR.'index.html';
+if ( !is_file($fp) ) touch($fp);
+
+$dir .= DIRECTORY_SEPARATOR.'config';
+if( !is_dir($dir) ) {
+    @mkdir($dir,0777,true);//OR 0775 or 0770
+    touch($dir.DIRECTORY_SEPARATOR.'index.html');
+}
