8eb8c47a5d012ee8fbc14ff20981e278e1deba08
tom2tom <tpgww@onepost.net>
2025-04-08 21:30:45 +1000

    disable duplication

--- admin/index.php
+++ admin/index.php
@@ -21,7 +21,7 @@ $orig_memory = (function_exists('memory_get_usage')?memory_get_usage():0);
 
 $CMS_ADMIN_PAGE=1;
 $CMS_TOP_MENU='main';
-$CMS_ADMIN_TITLE='adminhome';
+//$CMS_ADMIN_TITLE='adminhome'; repeated below
 $CMS_ADMIN_TITLE='mainmenu';
 $CMS_EXCLUDE_FROM_RECENT=1;
 
