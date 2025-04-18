56d11d20c07e2c0dfc7ba05e1aecd50515ee78a4
tom2tom <tpgww@onepost.net>
2025-04-18 19:35:11 +1000

    correct SQL

--- modules/DesignManager/action.admin_delete_template.php
+++ modules/DesignManager/action.admin_delete_template.php
@@ -48,8 +48,7 @@ try {
   }
 
   // find the number of 'pages' that use this template.
-  $db = cmsms()->GetDb();
-  $query = 'SELECT * FROM '.CMS_DB_PREFIX.'content WHERE template_id = ?';
+  $query = 'SELECT COUNT(*) FROM '.CMS_DB_PREFIX.'content WHERE template_id = ?';
   $n = $db->GetOne($query,array($tpl_ob->get_id()));
   $smarty->assign('page_usage',$n);
 
