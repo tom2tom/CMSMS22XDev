651226d6bd35a0554eec9ce595ec8d445b7b2325
tom2tom <tpgww@onepost.net>
2025-04-22 09:44:39 +1000

    use sql JOIN

--- modules/Search/action.keywords.php
+++ modules/Search/action.keywords.php
@@ -7,18 +7,17 @@ if( isset($params['count']) ) $wordcount = (int)$params['count'];
 $pageid = $returnid;
 if( isset($params['pageid']) ) $pageid = (int)$params['pageid'];
 
-$query = 'SELECT b.word
-            FROM '.CMS_DB_PREFIX.'module_search_items a,
-                 '.CMS_DB_PREFIX.'module_search_index b
-           WHERE a.content_id = \''.$pageid.'\'
-             AND a.module_name = \'search\'
-             AND a.extra_attr = \'content\'
-             AND a.id = b.item_id
-           ORDER BY b.count DESC';
-
-$dbr = $db->SelectLimit( $query, $wordcount, 0 );
+$query = 'SELECT b.word FROM '.
+CMS_DB_PREFIX.'module_search_items a INNER JOIN '.
+CMS_DB_PREFIX.'module_search_index b ON
+a.id = b.item_id
+WHERE a.content_id = \''.$pageid.'\'
+AND a.module_name = \'search\'
+AND a.extra_attr = \'content\'
+ORDER BY b.`count` DESC';
 
 $wordlist = array();
+$dbr = $db->SelectLimit( $query, $wordcount, 0 );
 if( $dbr ) {
     while( ($row = $dbr->FetchRow() ) ) {
         $wordlist[] = $row['word'];
