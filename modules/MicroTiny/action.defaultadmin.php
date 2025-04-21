bcf39431fbf596b224abdc9fcb5ba10a298e153a
tom2tom <tpgww@onepost.net>
2025-04-21 15:18:05 +1000

    setup for actual language-direction

--- modules/MicroTiny/action.defaultadmin.php
+++ modules/MicroTiny/action.defaultadmin.php
@@ -16,7 +16,18 @@
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 if( !cmsms() ) exit;
-if(!$this->VisibleToAdminUser() ) return;
+if( !$this->VisibleToAdminUser() ) return;
+
+//get a hint for floats inside the editor iframe (where sitewide css N/A)
+$lang = CmsNlsOperations::get_default_language();
+if( strncasecmp($lang, 'en', 2) == 0 ) {
+  $ldir = 'ltr';
+}
+else {
+  $obj = CmsNlsOperations::get_language_info($lang);
+  $ldir = (is_object($obj) && $obj->direction() == 'rtl') ? 'rtl' : 'ltr';
+}
+$seetab = (!empty($params['__activetab'])) ? $params['__activetab'] : '';
 
 $modname = $this->GetName();
 $tpl = $smarty->CreateTemplate("module_file_tpl:$modname;defaultadmin.tpl", null, $modname, $smarty);
@@ -24,6 +35,7 @@ $tpl = $smarty->CreateTemplate("module_file_tpl:$modname;defaultadmin.tpl", null
 //require __DIR__.DIRECTORY_SEPARATOR.'function.admin_example.php'; nothing in there
 require __DIR__.DIRECTORY_SEPARATOR.'function.admin_settings.php';
 
-$tpl->assign('tab', ''); // no tab-selection
+$tpl->assign('langside', $ldir);
+$tpl->assign('tab', $seetab);
 
 $tpl->display();
