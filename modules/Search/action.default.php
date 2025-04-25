84b995fc45f8a35659ecbd1d0554a93c8b17c17b
tom2tom <tpgww@onepost.net>
2025-04-25 12:29:48 +1000

    support placeholder specified in tag

--- modules/Search/action.default.php
+++ modules/Search/action.default.php
@@ -18,10 +18,7 @@ $compile_id = $this->GetName();
 
 $tpl = $smarty->CreateTemplate($this->GetTemplateResource($template),$cache_id,$compile_id,$smarty);
 if( !$tpl->IsCached() ) {
-    $inline = false;
-    if( isset( $params['inline'] ) ) {
-        $inline = cms_to_bool(trim($params['inline']));
-    }
+    $inline = ( isset($params['inline']) ) ? cms_to_bool(trim($params['inline'])) : false;
     $origreturnid = $returnid;
     if( isset( $params['resultpage'] ) ) {
         $manager = $gCms->GetHierarchyManager();
@@ -37,42 +34,36 @@ if( !$tpl->IsCached() ) {
     //Pretty URLs compatibility
     $is_method = (isset($params['search_method']))?'post':'get';
 
-    $searchtext = $this->GetPreference('searchtext','');
+    $searchtext = (!empty($params['searchtext'])) ? $params['searchtext'] : '';
+    if( !$searchtext ) {
+        $searchtext = $this->GetPreference('searchtext');
+    }
     if( !$searchtext ) {
         $searchtext = lang_by_realm('AdminSearch','placeholder_search_text');
         if( $searchtext && strpos($searchtext,'missing in the language') !== false ) {
             $searchtext = '';
         }
     }
-    $ph = ( $searchtext ) ? "placeholder=\"$searchtext\" " : '';
-    // Variable named hogan in honor of moorezilla's Rhodesian Ridgeback :) https://forum.cmsmadesimple.org/index.php/topic,9580.0.html
-    // some of it is only for backwards compatibility.
-    $hogan = "{$ph}onfocus=\"if(this.value==this.defaultValue) this.value='';\""." onblur=\"if(this.value=='') this.value=this.defaultValue;\"";
+
     $submittext = (!empty($params['submit'])) ? $params['submit'] : $this->Lang('searchsubmit');
-    $searchtext = (isset($params['searchtext'])) ? $params['searchtext'] : '';
+
+    $parms = [];
+    if( $origreturnid != $returnid ) { $parms['origreturnid'] = $origreturnid; }
+    if( isset($params['modules']) ) { $parms['modules'] = trim($params['modules']); }
+    if( isset($params['detailpage']) ) { $parms['detailpage'] = trim($params['detailpage']); }
+    foreach( $params as $key => $value ) {
+        if( preg_match('/^passthru_/', $key) ) { $parms[$key] = $value; }
+    }
 
     $tpl->assign('search_actionid',$id);
-    $tpl->assign('searchtext',$searchtext);
     $tpl->assign('destpage',$returnid);
     $tpl->assign('form_method',$is_method);
     $tpl->assign('inline',$inline);
-    $tpl->assign('startform',$this->CreateFormStart($id,'dosearch',$returnid,$is_method,'',$inline)); // TODO $hidden as form $params
-    $tpl->assign('label','<label for="'.$id.'searchinput">'.$this->Lang('search').'</label>');
+    $tpl->assign('startform',$this->CreateFormStart($id,'dosearch',$returnid,$is_method,'',$inline,'',$parms));
+    $tpl->assign('label','<label for="'.$id.'searchinput">'.$this->Lang('search').'</label>'); //better in the template, if always used !
     $tpl->assign('searchprompt',$this->Lang('search'));
-//  $tpl->assign('inputbox',$this->CreateInputText($id, 'searchinput', $searchtext, 20, 50, $hogan));
-//  $tpl->assign('submitbutton',$this->CreateInputSubmit($id, 'submit', $submittext));
+    $tpl->assign('searchtext',$searchtext);
     $tpl->assign('submittext',$submittext);
-    $tpl->assign('hogan',$hogan);
     $tpl->assign('endform',$this->CreateFormEnd());
-
-    $hidden = '';
-    if( $origreturnid != $returnid ) $hidden .= $this->CreateInputHidden($id, 'origreturnid', $origreturnid);
-    if( isset( $params['modules'] ) ) $hidden .= $this->CreateInputHidden( $id, 'modules', trim($params['modules']) );
-    if( isset( $params['detailpage'] ) ) $hidden .= $this->CreateInputHidden( $id, 'detailpage', trim($params['detailpage']) );
-    foreach( $params as $key => $value ) {
-        if( preg_match( '/^passthru_/', $key ) > 0 ) $hidden .= $this->CreateInputHidden($id,$key,$value);
-    }
-
-    if( $hidden ) $tpl->assign('hidden',$hidden);
 }
 $tpl->display();
