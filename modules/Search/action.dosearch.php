8f341277a8c209b1973344958d9eac15a49e60ee
tom2tom <tpgww@onepost.net>
2025-04-25 12:31:41 +1000

    conform to bool preference-values

--- modules/Search/action.dosearch.php
+++ modules/Search/action.dosearch.php
@@ -72,12 +72,12 @@ else {
 }
 $tpl = $smarty->CreateTemplate($this->GetTemplateResource($template),null,'Search',$smarty);
 
-if (isset($params['searchinput']) && $params['searchinput'] != '') {
+if( !empty($params['searchinput']) ) {
     // Fix to prevent XSS like behaviour. See: http://www.securityfocus.com/archive/1/455417/30/0/threaded
     //TODO robust sanitisation of user input c.f. news_ops::execSpecialize()
     $params['searchinput'] = cms_html_entity_decode($params['searchinput'],ENT_COMPAT,'UTF-8');
     $params['searchinput'] = strip_tags($params['searchinput']);
-    \CMSMS\HookManager::do_hook('Search::SearchInitiated', [trim($params['searchinput'])] );
+    CMSMS\HookManager::do_hook('Search::SearchInitiated', [trim($params['searchinput'])] );
 
     $searchstarttime = microtime(true);
 
@@ -87,19 +87,19 @@ if (isset($params['searchinput']) && $params['searchinput'] != '') {
     $max_weight = 1;
 
     $searchphrase = '';
-    if ($nb_words > 0) {
+    if( $nb_words > 0 ) {
         //$searchphrase = implode(' OR ', array_fill(0, $nb_words, 'word = ?'));
         $ary = array();
-        foreach ($words as $word) {
+        foreach( $words as $word ) {
             $word = trim($word);
             // $ary[] = "word = " . $db->qstr(htmlentities($word, ENT_COMPAT, 'UTF-8'));
             $ary[] = "word = " . $db->qstr($word);
         }
-        $searchphrase = implode(' OR ', $ary);
+        $searchphrase = implode(' OR ', $ary); //TODO IN query
     }
 
     // Update the search words table
-    if( $this->GetPreference('savephrases','false') == 'false' ) {
+    if( $this->GetPreference('savephrases', 0) == 0 ) {
         foreach( $words as $word ) {
             $q = 'SELECT `count` FROM '.CMS_DB_PREFIX.'module_search_words WHERE word = ?';
             $tmp = $db->GetOne($q,array($word));
@@ -128,9 +128,10 @@ if (isset($params['searchinput']) && $params['searchinput'] != '') {
     }
 
 //  $val = 100 * 100 * 100 * 100 * 25;
+    $longnow = $db->DBTimeStamp(time());
     $query = 'SELECT DISTINCT i.module_name, i.content_id, i.extra_attr, COUNT(*) AS nb, SUM(idx.`count`) AS total_weight FROM '.
     CMS_DB_PREFIX.'module_search_items i INNER JOIN '.
-    CMS_DB_PREFIX.'module_search_index idx ON idx.item_id = i.id WHERE (".$searchphrase.") AND (COALESCE(i.expires,NOW()) >= NOW())';
+    CMS_DB_PREFIX.'module_search_index idx ON idx.item_id = i.id WHERE ('.$searchphrase.") AND (COALESCE(i.expires,$longnow)>=$longnow)";
     if( isset( $params['modules'] ) ) {
         $modules = explode(",",$params['modules']);
         for( $i = 0; $i < count($modules); $i++ ) {
@@ -139,9 +140,9 @@ if (isset($params['searchinput']) && $params['searchinput'] != '') {
         $query .= ' AND i.module_name IN ('.implode(',',$modules).')';
     }
     $query .= ' GROUP BY i.module_name, i.content_id, i.extra_attr';
-    if( !isset($params['use_or']) || $params['use_or'] == 0 ) {
-        //This makes it an AND query
-        $query .= ' HAVING COUNT(*) >= $nb_words';
+    if( empty($params['use_or']) ) {
+        //This makes it an AND query TODO borks if $nb_words > 1
+        $query .= " HAVING nb >= $nb_words";
     }
     $query .= ' ORDER BY nb DESC, total_weight DESC';
 
@@ -217,7 +218,7 @@ if (isset($params['searchinput']) && $params['searchinput'] != '') {
     }
 
     $col->CalculateWeights();
-    if ($this->GetPreference('alpharesults', 'false') == 'true') $col->Sort();
+    if ($this->GetPreference('alpharesults', 0)) { $col->Sort(); }
 
     // now we're gonna do some post processing on the results
     // and replace the search terms with <span class="searchhilite">term</span>
