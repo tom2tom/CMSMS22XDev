d83eeb3ed9374682d680d2a516374c04e0045fda
tom2tom <tpgww@onepost.net>
2025-05-13 08:48:01 +1000

    add themes_url tag

---
+++ lib/plugins/function.themes_url.php
@@ -0,0 +1,38 @@
+<?php
+#Plugin handler: themes_url
+#(c) 2025 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+function smarty_function_themes_url($params, $smarty)
+{
+	$config = CmsApp::get_instance()->GetConfig();
+	$out = $config['themes_url'];
+	if( isset($params['assign']) ) {
+		$smarty->assign(trim($params['assign']),$out);
+		return '';
+	}
+	return $out;
+}
+
+function smarty_cms_about_function_themes_url()
+{
+?>
+	<p>Author: CMSMS devteam</p>
+	<p>Change History:</p>
+	<ul>
+		<li>None</li>
+	</ul>
+<?php
+}
