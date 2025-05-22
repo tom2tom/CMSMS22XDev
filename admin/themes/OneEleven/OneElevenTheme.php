8a6b29a0e6444a405e311b04fbb54dd660f6c254
tom2tom <tpgww@onepost.net>
2025-05-22 11:06:10 +1000

    typo

--- admin/themes/OneEleven/OneElevenTheme.php
+++ admin/themes/OneEleven/OneElevenTheme.php
@@ -11,12 +11,6 @@
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# However, as a special exception to the GPL, this software is distributed
-# as an addon module to CMS Made Simple.  You may not use this software
-# in any Non GPL version of CMS Made simple, or in any version of CMS
-# Made simple that does not indicate clearly and obviously in its admin
-# section that the site was built with CMS Made simple.
-#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -24,15 +18,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
-#
+# Or read it online: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #-------------------------------------------------------------------------
 
 use CMSMS\AdminAlerts\Alert;
 
-class OneElevenTheme extends CmsAdminThemeBase {
-	private $_errors = array();
-	private $_messages = array();
+class OneElevenTheme extends CmsAdminThemeBase
+{
 	// admin menu item names with corresponding topfiles icons
 	private $_topaliases = array (
 /* no top icon for:
@@ -362,7 +354,7 @@ class OneElevenTheme extends CmsAdminThemeBase {
 		$smarty->assign('secureparam', CMS_SECURE_PARAM_NAME . '=' . $_SESSION[CMS_USER_KEY]);
 		$userops = UserOperations::get_instance();
 		$smarty->assign('user', $userops->LoadUserByID($userid));
-		if ($this->errors && is_array($this->_errors)) { $smarty->assign('errors', $this->_errors); }
+		if ($this->_errors && is_array($this->_errors)) { $smarty->assign('errors', $this->_errors); }
 		if ($this->_messages && is_array($this->_messages)) { $smarty->assign('messages', $this->_messages); }
 
 		// is the website set down for maintenance?
