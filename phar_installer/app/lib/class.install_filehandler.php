da68f272a2f602db0f6c5d3f92ab4c6260d0175c
tom2tom <tpgww@onepost.net>
2025-05-22 12:09:54 +1000

    support placement of theme-related files in specified place

--- phar_installer/app/lib/class.install_filehandler.php
+++ phar_installer/app/lib/class.install_filehandler.php
@@ -8,6 +8,20 @@ use function __appbase\lang;
 
 class install_filehandler extends filehandler
 {
+  //the intended topmost dirpath for themes data
+  //same as or different from the installer-sources place (...assets/themes)
+  private $_themesdir;
+
+  public function set_themesdir($topdir)
+  {
+    if( !is_dir($topdir) ) {
+      @mkdir($topdir,0777,true);
+    }
+    if( !is_dir($topdir) || !is_writable($topdir) ) throw new Exception(lang('error_dirnotvalid',$topdir));
+    touch($topdir.'/index.html');
+    $this->_themesdir = $topdir;
+  }
+
   public function handle_file($filespec,$srcspec,PharFileInfo $fi)
   {
     if( $this->is_excluded($filespec) ) return;
@@ -27,6 +41,23 @@ class install_filehandler extends filehandler
         return;
       }
     }
+    elseif( $this->is_themefile($filespec) ) {
+      if( empty($this->_themesdir) ) throw new Exception(lang('error_nothemedir'));
+      $tp = $this->_themesdir;
+      $fp = $this->_destdir.'/assets/themes'; //default top-folder among installer files
+      $sp = substr($filespec,14); //ignore '/assets/themes' prefix
+      if( $tp != $fp ) {
+        $X = $fp.$sp;
+        if( is_file($X) ) {
+          unlink($X);
+        }
+      }
+      // if same file in some former place, also delete that
+      $X = $this->_destdir.'/uploads'.$sp;
+      if( is_file($X) ) {
+        unlink($X);
+      }
+    }
 
     if( !$this->dir_exists($filespec) ) {
       $this->create_directory($filespec);
@@ -43,6 +74,13 @@ class install_filehandler extends filehandler
       throw new Exception(lang('error_checksum',$filespec));
     }
     $this->output_string(lang('file_installed',$filespec));
+  }
+
+  private function is_themefile($filespec)
+  {
+    $filespec = ltrim($filespec);
+    if( !$filespec ) throw new Exception(lang('error_invalidparam','filespec'));
+    return strncmp($filespec,'/assets/themes/',15) == 0;
   }
 }
 
