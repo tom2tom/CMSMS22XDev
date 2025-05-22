036e075cc734490ab89b00533c7dd1dcc953d932
tom2tom <tpgww@onepost.net>
2025-05-22 12:18:24 +1000

    thow upon malformed xml

--- modules/DesignManager/lib/class.dm_reader_factory.php
+++ modules/DesignManager/lib/class.dm_reader_factory.php
@@ -15,14 +15,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
-#
+# Or read it online: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #-------------------------------------------------------------------------
 
 final class dm_reader_factory
 {
   private function __construct() {}
 
+  /**
+   * @param string $xmlfile filepath
+   * @return mixed dm_theme_reader object or dm_design_reader object
+   * @throws CmsFileSystemException
+   * @throws CmsException
+   */
   public static function get_reader($xmlfile)
   {
     $mod = cms_utils::get_module('DesignManager');
@@ -42,21 +47,12 @@ final class dm_reader_factory
     if( $p === FALSE ) throw new CmsException($mod->Lang('error_readxml'));  // highly unlikely.
     $word = substr($str,0,$p);
 
-    $ob = null; // default no object
     switch( $word ) {
     case 'theme':
-      $ob = new dm_theme_reader($xmlfile);
-      break;
-
+      return new dm_theme_reader($xmlfile);
     case 'design':
-      $ob = new dm_design_reader($xmlfile);
-      break;
+      return new dm_design_reader($xmlfile);
     }
-    return $ob;
+    throw new CmsException($mod->Lang('error_upload_filetype'));
   }
-} // end of class
-
-#
-# EOF
-#
-?>
+} // class
