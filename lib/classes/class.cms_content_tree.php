57ffe022d195650c9ef6ab9515ef1f5f8bf3bcc6
tom2tom <tpgww@onepost.net>
2025-06-25 09:56:32 +1000

    typo

--- lib/classes/class.cms_content_tree.php
+++ lib/classes/class.cms_content_tree.php
@@ -314,7 +314,7 @@ class cms_content_tree extends cms_tree
 	 * Get the children for this node.
 	 *
 	 * This method will retrieve a list of the children of this node, loading
-	 * their content objects at the same time (as a preformance enhancement).
+	 * their content objects at the same time (as a performance enhancement).
 	 *
 	 * This method takes advantage of the content cache.
 	 *
