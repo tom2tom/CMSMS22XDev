1774478b7adff478cd90b2ea24205f4d2778cb06
tom2tom <tpgww@onepost.net>
2025-06-29 12:50:24 +1000

    use internal PHP methods if possible

--- lib/smarty/functions.php
+++ lib/smarty/functions.php
@@ -12,40 +12,46 @@
 const SMARTY_HELPER_FUNCTIONS_LOADED = true;
 
 /**
- * Converts the first characters in $string to uppercase (A-Z) if it is an ASCII lowercase character (a-z).
- *
- * May not be required when running PHP8.2+: https://wiki.php.net/rfc/strtolower-ascii
+ * Converts the first character in $string to uppercase (A-Z) if it is an ASCII lowercase character (a-z).
  *
  * @param $string
  *
  * @return string
  */
-function smarty_ucfirst_ascii($string): string {
+function smarty_ucfirst_ascii($string): string
+{
+    if (PHP_VERSION_ID >= 80200) {
+        return ucfirst($string);
+    }
     return smarty_strtoupper_ascii(substr($string, 0, 1)) . substr($string, 1);
 }
 
 /**
  * Converts all uppercase ASCII characters (A-Z) in $string to lowercase (a-z).
  *
- * May not be required when running PHP8.2+: https://wiki.php.net/rfc/strtolower-ascii
- *
  * @param $string
  *
  * @return string
  */
-function smarty_strtolower_ascii($string): string {
+function smarty_strtolower_ascii($string): string
+{
+    if (PHP_VERSION_ID >= 80200) {
+        return strtolower($string);
+    }
     return strtr($string, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz');
 }
 
 /**
  * Converts all lowercase ASCII characters (a-z) in $string to uppercase (A-Z).
  *
- * May not be required when running PHP8.2+: https://wiki.php.net/rfc/strtolower-ascii
- *
  * @param $string
  *
  * @return string
  */
-function smarty_strtoupper_ascii($string): string {
+function smarty_strtoupper_ascii($string): string
+{
+    if (PHP_VERSION_ID >= 80200) {
+        return strtoupper($string);
+    }
     return strtr($string, 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 }
