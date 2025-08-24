258789ac1397125765c538099adbd9d6107e610b
tom2tom <tpgww@onepost.net>
2025-08-24 19:18:02 +1000

    extensive adjustments for latest upgrade

--- modules/News/method.upgrade.php
+++ modules/News/method.upgrade.php
@@ -1,4 +1,8 @@
 <?php
+#CMSMS News module method: upgrade
+#(c) 2004 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
+#The license at the top of file News.module.php applies to this file.
+
 if (!isset($gCms)) exit;
 
 if( version_compare($oldversion,'2.50') < 0 ) {
@@ -196,7 +200,7 @@ EOS;
     // correct and modify category-fields' format
     $when = $db->GetOne('SELECT MIN(create_date) FROM '.CMS_DB_PREFIX.'content WHERE create_date IS NOT NULL');
     if ($when) {
-        $ywhen = substr($when, 0, 11);
+        $ywhen = substr($when,0,11);
     }
     else {
         $ywhen = '2015-01-01 ';
@@ -214,19 +218,19 @@ EOS;
         $dc = $ywhen.$row['create_date'];
         if( $row['modified_date'] ) {
             $dm = $ywhen.$row['modified_date'];
-            if( strcmp($dm, $dc) < 0 ) {
+            if( strcmp($dm,$dc) < 0 ) {
                 $dm = $dc;
             }
         }
         else {
             $dm = null;
         }
-        $res = $db->Execute($query, array($dc, $dm, $row['news_category_id']));
+        $res = $db->Execute($query,array($dc,$dm,$row['news_category_id']));
     }
     $pref = CMS_DB_PREFIX;
-    foreach( array('news','news_categories','news_fielddefs','news_fieldvals') as $tn) {
+    foreach( array('news','news_categories','news_fielddefs','news_fieldvals') as $tname) {
         $query = <<<EOS
-ALTER TABLE `{$pref}module_{$tn}`
+ALTER TABLE `{$pref}module_{$tname}`
 CHANGE `create_date` `create_date` datetime NULL DEFAULT current_timestamp(),
 CHANGE `modified_date` `modified_date` datetime DEFAULT NULL ON UPDATE current_timestamp()
 EOS;
@@ -242,7 +246,178 @@ EOS;
         $this->SetPreference('fesubmit_redirect',$val);
     }
     elseif( $val != -1 ) {
-        $this->RemovePreference('fesubmit_redirect');
+        $node = $mgr->sureGetNodeById($val);
+        if( !$node ) {
+            $this->SetPreference('fesubmit_redirect',-1);
+        }
+    }
+}
+
+if( version_compare($oldversion,'2.51.16') < 0 ) {
+    // ensure recorded 'risky' properties are clean
+    $pref = CMS_DB_PREFIX;
+    $fmt = "UPDATE {$pref}module_news SET %s WHERE news_id=?";
+    $fu = ['news_title=?','news_data=?','summary=?','news_extra=?'];
+    $query = "SELECT news_id,news_title,news_data,summary,news_extra FROM {$pref}module_news";
+    $dbr = $db->GetArray($query);
+    foreach( $dbr as $row ) {
+        $FN = [];
+        $args = [];
+        $t = $row['news_title'];
+        $ta = ($t) ? news_ops::execSpecialize($t) : $t;
+        if( $ta != $t ) { $FN[] = $fu[0]; $args[] = $ta; }
+        $d = $row['news_data'];
+        $da = ($d) ? news_ops::execSpecialize($d) : $d;
+        if( $da != $d ) { $FN[] = $fu[1]; $args[] = $da; }
+        $s = $row['summary'];
+        $sa = ($s) ? news_ops::execSpecialize($s) : $s;
+        if( $sa != $s ) { $FN[] = $fu[2]; $args[] = $sa; }
+        $e = $row['news_extra'];
+        $ea = ($e) ? news_ops::execSpecialize($e) : $e;
+        if( $ea != $e ) { $FN[] = $fu[3]; $args[] = $ea; }
+        if( $FN ) {
+            $query = sprintf($fmt, implode(',',$FN));
+            $args[] = (int)$row['news_id'];
+            $db->Execute($query,$args);
+        }
+    }
+
+    // scrub redundant fields
+    $query = <<<EOS
+DELETE FV FROM {$pref}module_news_fieldvals FV
+JOIN {$pref}module_news_fielddefs FD ON FV.fielddef_id = FD.id
+WHERE (FV.value IS NULL OR FV.value='' OR (FV.value=-1 AND FD.type='dropdown'))
+EOS;
+    $db->Execute($query);
+
+    // re-arrange maximum length
+    $dbr = $db->GetArray("SELECT id,type,max_length,extra FROM {$pref}module_news_fielddefs");
+    $query = "UPDATE {$pref}module_news_fielddefs SET extra=? WHERE id=?";
+    foreach( $dbr as $row ) {
+       switch ($row['type']) {
+           case 'dropdown':
+               continue 2; // stet this one
+           case 'textbox':
+               $lm = (int)$row['max_length'];
+               if ($lm < 48) { $lm = 48; }
+               $adjusted = serialize(['max_length'=>$lm]);
+               break;
+           default:
+               $adjusted = null; // clear it
+               break;
+       }
+       if ($adjusted !== false) $db->Execute($query,[$adjusted,$row['id']]);
+    }
+
+$query = <<<EOS
+ALTER TABLE `{$pref}module_news_fielddefs`
+CHANGE `type` `type` varchar(24) CHARACTER SET ascii COLLATE ascii_bin,
+CHANGE `item_order` `item_order` smallint unsigned,
+CHANGE `public` `public` tinyint unsigned
+EOS;
+    $db->Execute($query);
+
+    if( !isset($dict) ) { $dict = NewDataDictionary($db); }
+    $sqlarray = $dict->DropColumnSQL("{$pref}module_news_fielddefs",['max_length']);
+    $dict->ExecuteSQLArray($sqlarray);
+
+    // sanitize risky values in fieldvals
+    $query = <<<EOS
+SELECT FV.news_id,FV.fielddef_id,FV.value FROM {$pref}module_news_fieldvals FV
+JOIN {$pref}module_news_fielddefs FD ON FV.fielddef_id = FD.id
+WHERE (FD.type='textbox' OR FD.type='textarea')
+EOS;
+    $dbr = $db->GetArray($query);
+    foreach( $dbr as $row ) {
+        $t = $row['value'];
+        $ta = ($t) ? news_ops::execSpecialize($t) : $t;
+        if( $ta != $t ) {
+            $db->Execute("UPDATE {$pref}module_news_fieldvals SET value=? WHERE news_id=? AND fielddef_id=?",
+                [$ta,(int)$row['news_id'],(int)$row['fielddef_id']]);
+        }
+    }
+
+    // update default templates
+    $now = time();
+    $done = 0;
+    $query = 'INSERT INTO '.CMS_DB_PREFIX.'layout_templates
+(name,content,description,type_id,type_dflt,category_id,owner_id,listable,created,modified)
+VALUES (?,?,?,?,?,?,?,?,?,?)';
+    $outfiles = [
+     'Simplex_Detail_template',
+     'orig_detail_template',
+     'orig_form_template',
+     'orig_summary_template'
+    ];
+    $innames = [
+     'Simplex News Detail',
+     'News Detail Sample',
+     'News Fesubmit Form Sample',
+     'News Summary Sample'
+    ];
+    foreach( $innames as $i => $tplname ) {
+        $newcontent = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.$outfiles[$i].'.tpl');
+        $row = $db->GetRow("SELECT * FROM {$pref}layout_templates WHERE name = ?",[$tplname]);
+        if( $row ) {
+            $args = $row;
+            unset($args['id']);
+            $args['name'] .= ' Superseded Content';
+            $args['type_dflt'] = 0;
+            $db->Execute($query,array_values($args));
+
+            $tplid = $row['id'];
+            $db->Execute("UPDATE {$pref}layout_templates SET content=?,modified=$now WHERE id=$tplid",[$newcontent]);
+            if( $row['type_dflt'] ) {
+                $type = CmsLayoutTemplateType::load($row['type_id']);
+                $type->reset_content_to_factory();
+            }
+            ++$done;
+        }
+        else {
+            // record new template as example
+            $desc = 'Default News template example';
+            $ttid = 999;
+            $args = [$tplname.' Replaced Content',$newcontent,$desc,$ttid,0,0,1,1,$now,$now];
+            $db->Execute($query,$args);
+        }
+    }
+    if( $done > 0 ) {
+        audit('','News module',"Replaced content of $done recorded template(s) involving custom fields");
+        $msg = <<<EOS
+The content of $done News-module default templates (involving custom fields) has been replaced.
+Any customisation formerly in those will need to be re-created from the backup templates.
+EOS;
+        if( $done == 1 ) {
+            $msg = str_replace(['lates','ose'],['late','at'],$msg);
+        }
+        $themeObject = cms_utils::get_theme_object();
+        $themeObject->AddNotification(2,'News module change',$msg);
+    }
+
+/*  // conform uploads folder to module name
+    $from = $config['uploads_path'].DIRECTORY_SEPARATOR.'news';
+    if( is_dir($from) ) {
+        $to = str_replace('news',$this->GetName(),$from);
+        rename($from,$to);
+    }
+    $query = "UPDATE {$pref}module_news_fieldvals SET value=? WHERE value LIKE '%news%'" i.e. rename news[\/] to News[\/] whereever used
+    $db->Execute($query);
+*/
+    // rename preference
+    $t = $this->GetPreference('formsubmit_emailaddress','^_NONE_^');
+    if( $t != '^_NONE_^' ) {
+        $this->RemovePreference('formsubmit_emailaddress');
+        $this->SetPreference('fesubmit_emailaddress',$t);
+    }
+
+    // relocate/install plugin
+    $from = __DIR__.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'function.news_image.php';
+    $to = cms_join_path(CMS_ROOT_PATH,'lib','plugins','function.news_image.php');
+    if( copy($from,$to) ) {
+        chmod($to,0644);
+    }
+    else {
+        audit('','News module','Failed to install news_image plugin');
     }
 }
 
