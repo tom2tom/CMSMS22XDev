53a0d19e8fb8c87abc11c1d5c340ec8bf06a375b
tom2tom <tpgww@onepost.net>
2025-08-24 13:04:20 +1000

    file-selector inside <div/>

--- modules/FilePicker/action.select.php
+++ modules/FilePicker/action.select.php
@@ -5,15 +5,15 @@ if( CmsApp::get_instance()->is_frontend_request() ) exit;
 try {
     $name = get_parameter_value($params,'name');
     $value = get_parameter_value($params,'value');
-    $type = get_parameter_value($params,'type'); //WHAT??
-    $type = 'image'; //WHAT??
+    $type = get_parameter_value($params,'type','image');
+//  $type = 'image'; was intended default ? if so include in previous line
 
     $profile = $this->get_default_profile();
-    if( $type ) {
-        $parms = [ 'type' => $type ];
-        $profile = $profile->overrideWith( $parms );
-    }
-    echo $this->get_html($name, $value, $profile );
+//  if( $type ) {
+        $parms = ['type' => $type];
+        $profile = $profile->overrideWith($parms);
+//  }
+    echo "<div>\n".$this->get_html($name,$value,$profile)."\n</div>";
 }
 catch( \Exception $e ) {
     echo $this->ShowErrors($e->GetMessage());
