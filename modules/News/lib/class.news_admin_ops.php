1bb4176f0e274a85d4b8975dda6c51c53edf464f
tom2tom <tpgww@onepost.net>
2025-08-24 19:16:33 +1000

    improve processing of uploaded files

--- modules/News/lib/class.news_admin_ops.php
+++ modules/News/lib/class.news_admin_ops.php
@@ -1,106 +1,93 @@
 <?php
-#Module News class news_admin_ops
+#CMSMS News module class: news_admin_ops
 #(c) 2004 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
-#
-#This program is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 2 of the License, or
-#(at your option) any later version.
-#
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-#$Id$
+#The license at the top of file News.module.php applies to this file.
 
 use CMSMS\HookManager;
 
 final class news_admin_ops
 {
-    protected function __construct() {}
-
     public static function delete_article($articleid)
     {
-        HookManager::do_hook('News::NewsArticleDeletedPre', ['news_id'=>$articleid ]);
+        HookManager::do_hook('News::NewsArticleDeletedPre', ['news_id'=>$articleid]);
 
         $db = cmsms()->GetDb();
 
-        //Now remove the article
+        // Remove the article
         $query = "DELETE FROM ".CMS_DB_PREFIX."module_news WHERE news_id = ?";
         $db->Execute($query, array($articleid));
 
-        // Delete it from the custom fields
+        // Delete any associated custom fields
         $query = 'DELETE FROM '.CMS_DB_PREFIX.'module_news_fieldvals WHERE news_id = ?';
         $db->Execute($query, array($articleid));
 
-        // delete any files...
+        // Delete any uploads for 'file' fields
         $config = cmsms()->GetConfig();
-        $p = cms_join_path($config['uploads_path'],'news','id'.$articleid);
-        if( is_dir($p) ) recursive_delete($p);
+        $p = cms_join_path($config['uploads_path'], 'news', 'id'.$articleid);
+        if (is_dir($p)) recursive_delete($p);
 
         news_admin_ops::delete_static_route($articleid);
 
-        //Update search index
+        // Update search index
         $mod = cms_utils::get_module('News');
         $module = cms_utils::get_search_module();
         if ($module) $module->DeleteWords($mod->GetName(), $articleid, 'article');
 
-        HookManager::do_hook('News::NewsArticleDeleted', ['news_id'=>$articleid ]);
+        HookManager::do_hook('News::NewsArticleDeleted', ['news_id'=>$articleid]);
 
-        // put mention into the admin log
+        // Put mention into the admin log
         audit($articleid, $mod->GetName().' article', "Deleted: $articleid");
     }
 
-    public static function handle_upload($itemid,$fieldname,&$error)
+    public static function handle_upload($itemid, $fieldname, &$error)
     {
         $config = cmsms()->GetConfig();
 
         $mod = cms_utils::get_module('News');
-        $p = cms_join_path($config['uploads_path'],'news');
-        if (!is_dir($p)) {
-            $res = @mkdir($p);
-            if( $res === FALSE ) {
-                $error = $mod->Lang('error_mkdir',$p);
-                return FALSE;
-            }
-        }
-
-        $p = cms_join_path($config['uploads_path'],'news','id'.$itemid);
-        if (!is_dir($p)) {
-            if( @mkdir($p) === FALSE ) {
-                $error = $mod->Lang('error_mkdir',$p);
-                return FALSE;
-            }
-        }
-
-        if( $_FILES[$fieldname]['size'] > $config['max_upload_size'] ) {
+        if ($_FILES[$fieldname]['size'] > $config['max_upload_size']) {
             $error = $mod->Lang('error_filesize');
             return FALSE;
         }
 
         $filename = basename($_FILES[$fieldname]['name']);
-        $dest = cms_join_path($config['uploads_path'],'news','id'.$itemid,$filename);
 
-        // Get the files extension
-        $ext = substr(strrchr($filename, '.'), 1);
+        // Get the file extension
+        $xp = (int)strrpos($filename, '.');
+        $ext = ($xp > 0) ? strtolower(substr($filename, $xp + 1)) : ''; // hidden files ignored
 
-        // compare it against the 'allowed extentions'
-        $exts = explode(',',$mod->GetPreference('allowed_upload_types',''));
-        if( !in_array( $ext, $exts ) )  {
+        // Compare it against allowed extensions
+        $exts = explode(',', $mod->GetPreference('allowed_upload_types', ''));
+        if ($exts && !in_array($ext, array_map('strtolower', $exts))) {
             $error = $mod->Lang('error_invalidfiletype');
             return FALSE;
         }
 
-        if( @cms_move_uploaded_file($_FILES[$fieldname]['tmp_name'], $dest) === FALSE ) {
-            $error = $mod->Lang('error_movefile',$dest);
-            return FALSE;
+        $p = $config['uploads_path'].DIRECTORY_SEPARATOR.'news'.DIRECTORY_SEPARATOR.'id'.$itemid; //module uploads folder created on-demand
+        if (!is_dir($p)) {
+            if (!@mkdir($p, 0777, TRUE)) {
+                $error = $mod->Lang('error_mkdir', $p);
+                return FALSE;
+            }
+            touch($p.DIRECTORY_SEPARATOR.'index.html');
+            $dir = $p;
+            while ($dir != $config['uploads_path']) {
+                $dir = dirname($dir);
+                $fp = $dir.DIRECTORY_SEPARATOR.'index.html';
+                if (is_file($fp)) {
+                    break;
+                }
+                touch($fp);
+            }
         }
 
-        return $filename;
+        $dest = $p.DIRECTORY_SEPARATOR.$filename;
+        if (!@cms_move_uploaded_file($_FILES[$fieldname]['tmp_name'], $dest)) {
+            $error = $mod->Lang('error_movefile', $dest);
+            return FALSE;
+        }
+        chmod($dest, 0644);
+
+        return $filename; // its path will be handled in-context
     }
 
     public static function UpdateHierarchyPositions()
@@ -109,20 +96,19 @@ final class news_admin_ops
 
         $query = "SELECT news_category_id, item_order, news_category_name FROM ".CMS_DB_PREFIX."module_news_categories";
         $dbresult = $db->Execute($query);
-        if( $dbresult ) {
+        if ($dbresult) {
             $query = "SELECT news_category_id, item_order, news_category_name, parent_id FROM ".CMS_DB_PREFIX."module_news_categories WHERE news_category_id = ?";
             $stmt = $db->Prepare("UPDATE ".CMS_DB_PREFIX."module_news_categories SET hierarchy = ?, long_name = ? WHERE news_category_id = ?");
-            while( $row = $dbresult->FetchRow() ) {
+            while ($row = $dbresult->FetchRow()) {
                 $current_hierarchy_position = "";
                 $current_long_name = "";
                 $content_id = $row['news_category_id'];
                 $current_parent_id = $row['news_category_id'];
                 $count = 0;
 
-                while( $current_parent_id > -1 ) {
+                while ($current_parent_id > -1) {
                     $row2 = $db->GetRow($query, array($current_parent_id));
-                    if( $row2 ) {
-                        //TODO prevent item_order null values ?
+                    if ($row2) {
                         //3-wide hierarchy positions are more than enough
                         $current_hierarchy_position = str_pad((string)$row2['item_order'], 3, '0', STR_PAD_LEFT) . "." . $current_hierarchy_position;
                         $current_long_name = $row2['news_category_name'] . ' | ' . $current_long_name;
@@ -134,11 +120,11 @@ final class news_admin_ops
                     }
                 }
 
-                if( ($l = strlen($current_hierarchy_position)) > 0 ) {
+                if (($l = strlen($current_hierarchy_position)) > 0) {
                     $current_hierarchy_position = substr($current_hierarchy_position, 0, $l - 1);
                 }
 
-                if( ($l = strlen($current_long_name)) > 0 ) {
+                if (($l = strlen($current_long_name)) > 0) {
                     $current_long_name = substr($current_long_name, 0, $l - 3);
                 }
 
@@ -150,39 +136,39 @@ final class news_admin_ops
 
     public static function delete_static_route($news_article_id)
     {
-        return cms_route_manager::del_static('','News',$news_article_id);
+        return cms_route_manager::del_static('', 'News', $news_article_id);
     }
 
-    public static function register_static_route($news_url,$news_article_id,$detailpage = 0)
+    public static function register_static_route($news_url, $news_article_id, $detailpage = 0)
     {
-        if( $detailpage <= 0 ) {
+        if ($detailpage <= 0) {
             $module = cms_utils::get_module('News');
-            $detailpage = $module->GetPreference('detail_returnid',-1);
-            if( $detailpage == -1 ) {
+            $detailpage = $module->GetPreference('detail_returnid', -1);
+            if ($detailpage == -1) {
                 $gCms = cmsms();
                 $detailpage = $gCms->GetContentOperations()->GetDefaultContent();
             }
         }
-        $parms = array('action'=>'detail','returnid'=>$detailpage,'articleid'=>$news_article_id);
-        $route = CmsRoute::new_builder($news_url,'News',$news_article_id,$parms,TRUE);
+        $parms = array('action'=>'detail', 'returnid'=>$detailpage, 'articleid'=>$news_article_id);
+        $route = CmsRoute::new_builder($news_url, 'News', $news_article_id, $parms, TRUE);
         return cms_route_manager::add_static($route);
     }
 
     public static function optionstext_to_array($txt)
     {
         $txt = trim($txt);
-        if( !$txt ) return [];
+        if (!$txt) return [];
 
         $arr_options = array();
-        $tmp1 = explode("\n",$txt);
-        foreach( $tmp1 as $tmp2 ) {
+        $tmp1 = explode("\n", $txt);
+        foreach ($tmp1 as $tmp2) {
             $tmp2 = trim($tmp2);
-            if( $tmp2 == '' ) continue;
+            if ($tmp2 == '') continue;
             $tmp2_k = $tmp2_v = $tmp2;
-            if( strpos($tmp2,'=') !== FALSE ) {
-                list($tmp2_k,$tmp2_v) = explode('=',$tmp2,2);
+            if (strpos($tmp2, '=') !== FALSE) {
+                list($tmp2_k, $tmp2_v) = explode('=', $tmp2, 2);
             }
-            if( $tmp2_k == '' || $tmp2_v == '' ) continue;
+            if ($tmp2_k == '' || $tmp2_v == '') continue;
             $arr_options[$tmp2_k] = $tmp2_v;
         }
         return $arr_options;
@@ -191,7 +177,7 @@ final class news_admin_ops
     public static function array_to_optionstext($arr)
     {
         $txt = '';
-        foreach( $arr as $key => $val ) {
+        foreach ($arr as $key => $val) {
             $txt .= "$key=$val\n";
         }
         return trim($txt);
