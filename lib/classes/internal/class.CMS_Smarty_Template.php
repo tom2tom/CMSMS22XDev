ceb8ace3fcf37ee76ce571d77d44a4b27daffc0a
tom2tom <tpgww@onepost.net>
2025-08-24 13:00:24 +1000

    add method display()

--- lib/classes/internal/class.CMS_Smarty_Template.php
+++ lib/classes/internal/class.CMS_Smarty_Template.php
@@ -14,18 +14,23 @@ class CMS_Smarty_Template extends Smarty_Internal_Template
         if( is_null($cache_id) || $cache_id === '' ) {
             $cache_id = $global_cache_id;
         }
-        else if( $cache_id[0] == '|' ) {
+        elseif( $cache_id[0] == '|' ) {
             $cache_id = $global_cache_id . $cache_id;
         }
 
-        if (!$template) {
+        if( !$template ) {
             $template = $this;
         }
         // send an event before fetching...this allows us to change template stuff.
         if( CmsApp::get_instance()->is_frontend_request() ) {
-            $parms = array('template'=>&$template,'cache_id'=>&$cache_id,'compile_id'=>&$compile_id,'display'=>&$display);
-            \CMSMS\HookManager::do_hook( 'Core::TemplatePrefetch', $parms );
+            $parms = array('template'=>&$template,'cache_id'=>&$cache_id,'compile_id'=>&$compile_id);
+            \CMSMS\HookManager::do_hook( 'Core::TemplatePreFetch', $parms );
         }
         return parent::fetch($template,$cache_id,$compile_id,$parent);
+    }
+
+    public function display($template = null, $cache_id = null, $compile_id = null, $parent = null)
+    {
+        echo $this->fetch($template,$cache_id,$compile_id,$parent);
     }
 }
