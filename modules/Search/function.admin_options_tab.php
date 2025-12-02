802188bd63cf17f135c52bc23a7f388eab43a1e7
tom2tom <tpgww@onepost.net>
2025-12-03 09:32:37 +1100

    migrate content-generation to template

--- modules/Search/function.admin_options_tab.php
+++ modules/Search/function.admin_options_tab.php
@@ -3,44 +3,12 @@
 $tpl->assign('oformstart',
 			$this->CreateFormStart($id, 'defaultadmin', $returnid, 'post', '',
 								   false, '', array('__activetab'=>'settings')));
-$tpl->assign('reindex', $this->CreateInputSubmit($id, 'reindex', $this->Lang('reindexallcontent')));
-$tpl->assign('prompt_stopwords', $this->Lang('stopwords'));
-$tpl->assign('input_stopwords',
-			$this->CreateTextArea(false, $id, str_replace(array("\r", "\n"), '',
-								  $this->GetPreference('stopwords', $this->DefaultStopWords())),
-								  'stopwords', '', '', '', '', '50', '6'));
-$tpl->assign('prompt_resetstopwords', $this->Lang('prompt_resetstopwords'));
-$tpl->assign('input_resetstopwords', $this->CreateInputSubmit($id, 'resettodefault', $this->Lang('input_resetstopwords')));
-
-$tpl->assign('prompt_stemming', $this->Lang('usestemming'));
-$tpl->assign('input_stemming',
-			$this->CreateInputCheckbox($id, 'usestemming', '1',
-									   $this->GetPreference('usestemming', '0'),
-									   'id="chkstem"'));
-
-$tpl->assign('prompt_searchtext', $this->Lang('prompt_searchtext'));
-$tpl->assign('input_searchtext',
-			$this->CreateInputText($id, 'searchtext',
-								   $this->GetPreference('searchtext'),
-								   '15', '64'));
-
-$tpl->assign('prompt_savephrases', $this->Lang('prompt_savephrases'));
-$tpl->assign('input_savephrases',
-			$this->CreateInputCheckbox($id, 'savephrases', '1',
-									   $this->GetPreference('savephrases', '0'),
-									  'id="chkphrased"'));
-
-$tpl->assign('prompt_alpharesults', $this->Lang('prompt_alpharesults'));
-$tpl->assign('input_alpharesults',
-			$this->CreateInputCheckbox($id, 'alpharesults', '1',
-									   $this->GetPreference('alpharesults', '0'),
-									   'id="chkalpha"'));
-
-$tpl->assign('prompt_resultpage', $this->Lang('prompt_resultpage'));
-/*
-$contentops = $gCms->GetContentOperations();
-$tpl->assign('input_resultpage',
-			$contentops->CreateHierarchyDropdown(0,
-								$this->GetPreference('resultpage', -1),
-								$id.'resultpage', true));
-*/
+$stops = $this->GetPreference('stopwords');
+if (!$stops) {
+	$stops = $this->DefaultStopWords();
+}
+$tpl->assign('current_stopwords', str_replace(["\r\n", "\n", "\r"], ['', '',''], $stops));
+$tpl->assign('search_text', $this->GetPreference('searchtext'));
+$tpl->assign('use_stemming',$this->GetPreference('usestemming', '0'));
+$tpl->assign('save_phrases', $this->GetPreference('savephrases', '0'));
+$tpl->assign('alpha_results', $this->GetPreference('alpharesults', '0'));
--- modules/Search/function.admin_statistics_tab.php
+++ modules/Search/function.admin_statistics_tab.php
@@ -8,13 +8,6 @@ if( $this->GetPreference('savephrases', 0) ) {
 	$title = $this->Lang('word');
 }
 $tpl->assign('wordtext',$title);
-$tpl->assign('counttext',$this->Lang('count'));
-$tpl->assign('exportcsv',
-		$this->CreateInputSubmit($id,'exportcsv',$this->Lang('export_to_csv'),
-			'data-ui-icon="ui-icon-arrowreturnthick-1-s"'));
-$tpl->assign('clearwordcount',
-		$this->CreateInputSubmit($id,'clearwordcount',$this->Lang('clear'),
-			'data-ui-icon="ui-icon-minusthick"','',$this->Lang('confirm_clearstats')));
 
 $results = array();
 $query = 'SELECT * FROM '.CMS_DB_PREFIX.'module_search_words ORDER BY `count` DESC';
