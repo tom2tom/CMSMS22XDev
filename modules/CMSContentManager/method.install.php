e14d0c3a179f57981d57bb2bc2e9fa01f5bb66f7
tom2tom <tpgww@onepost.net>
2025-12-18 18:08:37 +1100

    conform license text

--- modules/CMSContentManager/method.install.php
+++ modules/CMSContentManager/method.install.php
@@ -1,33 +1,8 @@
 <?php
-#BEGIN_LICENSE
-#-------------------------------------------------------------------------
-# Module CMSContentManager installation
-# (c) 2013 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
-#
-#-------------------------------------------------------------------------
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# However, as a special exception to the GPL, this software is distributed
-# as an addon module to CMS Made Simple.  You may not use this software
-# in any Non GPL version of CMS Made simple, or in any version of CMS
-# Made simple that does not indicate clearly and obviously in its admin
-# section that the site was built with CMS Made simple.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
-#
-#-------------------------------------------------------------------------
-#END_LICENSE
+#CMSMS CMSContentManager module method: install
+#(c) 2013 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
+#The license at the top of file CMSContentManager.module.php applies to this file.
+
 if( !isset($gCms) ) exit;
 
 $this->SetPreference('locktimeout',60);
@@ -39,7 +14,4 @@ $this->SetPreference('order_TAB_LOGIC',6);
 $this->SetPreference('order_TAB_OPTIONS',8);
 $this->SetPreference('order_TAB_PERMS',10);
 
-#
-# EOF
-#
 ?>
