ee1d940d99a1521bf6f9ce4f7014ca7acf789fcd
tom2tom <tpgww@onepost.net>
2026-02-20 15:19:03 +1100

    use namespaced object

--- modules/CmsJobManager/action.test1.php
+++ modules/CmsJobManager/action.test1.php
@@ -1,9 +1,17 @@
 <?php
+/*
+CMSMS CmsJobManager module action: test1
+(C) 2016 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
+The license at the top of file CmsJobManager.module.php applies to this file.
+*/
+// This action is for use when developing, debugging async processing
+// Normally there's no UI engagement with this
+
 if( !isset($gCms) ) exit;
 if( !$this->VisibleToAdminUser() ) exit;
 
-$newjob = new Test1Job();
+$newjob = new CmsJobManager\Test1Job();
 $newjob->save();
 
-$this->SetMessage('Job Created');
-$this->RedirectToAdminTab();
+$this->SetMessage('Job created');
+$this->Redirect($id, 'defaultadmin');
--- modules/CmsJobManager/action.test2.php
+++ modules/CmsJobManager/action.test2.php
@@ -1,9 +1,17 @@
 <?php
+/*
+CMSMS CmsJobManager module action: test2
+(C) 2016 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
+The license at the top of file CmsJobManager.module.php applies to this file.
+*/
+// This action is for use when developing, debugging async processing
+// Normally there's no UI engagement with this
+
 if( !isset($gCms) ) exit;
 if( !$this->VisibleToAdminUser() ) exit;
 
-$newjob = new Test1Cron();
+$newjob = new CmsJobManager\Test1Cron();
 $newjob->save();
 
-$this->SetMessage('Job Created');
-$this->RedirectToAdminTab();
+$this->SetMessage('Cron Job created');
+$this->Redirect($id, 'defaultadmin');
