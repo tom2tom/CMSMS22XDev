9b7d7832cd6f243db6ca17f47b09758d660d20db
tom2tom <tpgww@onepost.net>
2026-02-20 15:18:12 +1100

    extra removals

--- modules/CmsJobManager/method.uninstall.php
+++ modules/CmsJobManager/method.uninstall.php
@@ -1,10 +1,25 @@
 <?php
-if( !isset($gCms) ) exit;
+/*
+CMSMS CmsJobManager module uninstallation script
+(C) 2016 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
+The license at the top of file CmsJobManager.module.php applies to this file.
+*/
 
-$dict = NewDataDictionary( $db );
-$sqlarray = $dict->DropTableSQL( CmsJobManager::table_name() );
+use CMSMS\Async\Job;
+
+if( !isset($gCms) ) exit;
+if( !$this->CheckPermission('Modify Modules') ) exit;
+
+$dict = NewDataDictionary($db);
+$tbl = CMS_DB_PREFIX.Job::RECORDTABLE;
+$sqlarray = $dict->DropTableSQL($tbl);
+$dict->ExecuteSQLArray($sqlarray);
+$db->DropSequence("{$tbl}_seq");
+$sqlarray = $dict->DropTableSQL(CMS_DB_PREFIX.Job::CACHETABLE);
 $dict->ExecuteSQLArray($sqlarray);
 
-$this->RemovePermission(\CmsJobManager::MANAGE_JOBS);
-$this->RemoveEvent(\CmsJobManager::EVT_ONFAILEDJOB);
+cms_siteprefs::remove('JobLastrun:',true);
+$this->RemovePreference();
+//$this->RemovePermission(CmsJobManager::MANAGE_JOBS);
+$this->RemoveEvent(CmsJobManager::EVT_ONFAILEDJOB);
 $this->RemoveEventHandler('Core','ModuleUninstalled');
