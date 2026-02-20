fa21ddc032dc5170c07ec84447e6b7cb0c8ab1d1
tom2tom <tpgww@onepost.net>
2026-02-20 15:21:03 +1100

    conform to current API

--- modules/CmsJobManager/lib/class.Test1Cron.php
+++ modules/CmsJobManager/lib/class.Test1Cron.php
@@ -1,4 +1,11 @@
 <?php
+/*
+CMSMS CmsJobManager module class: Test1Cron
+(C) 2016 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
+The license at the top of file CmsJobManager.module.php applies to this file.
+*/
+
+namespace CmsJobManager;
 
 use CMSMS\Async\CronJob;
 
@@ -7,15 +14,17 @@ class Test1Cron extends CronJob
     public function __construct()
     {
         parent::__construct();
+        $this->name = 'Test1Cron';
         $this->module = 'CmsJobManager';
-        $this->frequency = self::RECUR_HOURLY;
+        $this->description = 'Creates an admin log entry hourly';
+        $this->recurs = parent::RECUR_HOURLY;
         $this->until = strtotime('+1 day');
     }
 
-    public function execute()
+    public function execute($now = 0)
     {
-        // simple test, creates an audit string
-        //some_unknown_function(); // intentionally generate an error.
+        $this->record_exec_time($now);
+//      some_unknown_function(); // intentionally generate an error
         audit('',$this->module,'Cron job Test1 complete');
         debug_to_log('Cron job Test1 complete');
     }
--- modules/CmsJobManager/lib/class.Test1Job.php
+++ modules/CmsJobManager/lib/class.Test1Job.php
@@ -1,4 +1,12 @@
 <?php
+/*
+CMSMS CmsJobManager module class: Test1Job
+(C) 2016 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
+The license at the top of file CmsJobManager.module.php applies to this file.
+*/
+
+namespace CmsJobManager;
+//namespace CMSMS\tasks; if directly installed for testing
 
 use CMSMS\Async\Job;
 
@@ -7,13 +15,16 @@ class Test1Job extends Job
     public function __construct()
     {
         parent::__construct();
-        $this->module = 'CmsJobManager';
+        $this->name = 'Test1Job';
+        $this->module = 'CmsJobManager'; // if not directly installed
+        $this->description = 'Creates an admin log entry';
+        $this->start = strtotime('+1 day',time());
     }
 
-    public function execute()
+    public function execute($now = 0)
     {
-        // simple test, creates an audit string
-        //some_unknown_function(); // intentionally generate an error.
+        $this->record_exec_time($now);
+//      some_unknown_function(); // intentionally generate an error
         audit('',$this->module,'Job Test1 complete');
         debug_to_log('Job Test1 complete');
     }
