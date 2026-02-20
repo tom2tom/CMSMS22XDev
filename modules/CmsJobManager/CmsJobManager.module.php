b1e9be7b89ea7fa2b5deaafeff5d7bd5679b3631
tom2tom <tpgww@onepost.net>
2026-02-20 15:17:29 +1100

    version 0.2 export code to classes and actions remove permission

--- modules/CmsJobManager/CmsJobManager.module.php
+++ modules/CmsJobManager/CmsJobManager.module.php
@@ -16,300 +16,222 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
-#
+# along with this program. If not, read the license online at:
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #-------------------------------------------------------------------------
 #END_LICENSE
 
-use CMSMS\Async\Job as Job;
-//use CMSMS\Async\CronJobTrait;
+use CMSMS\Async\Job;
+use CMSMS\JobOperations;
 
-final class CmsJobManager extends \CMSModule
+final class CmsJobManager extends CMSModule
 {
-    const LOCKPREF = 'lock';
-    const ASYNCFREQ_PREF = 'asyncfreq';
-    const MANAGE_JOBS = 'Manage Jobs';
-    const EVT_ONFAILEDJOB = 'CmsJobManager::OnJobFailed';
+    const EVT_ONFAILEDJOB = 'OnJobFailed';
+    const LOCKPREF = 'jobs_lock';
 
-    private $_current_job;
-    private $_lock;
-
-    public static function table_name() { return CMS_DB_PREFIX.'mod_cmsjobmgr'; }
+    private $_lock = 0; // timestamp (replicated in module-preference LOCKPREF)
 
     public function GetFriendlyName() { return $this->Lang('friendlyname'); }
-    public function GetVersion() { return '0.1.4'; }
-    public function MinimumCMSVersion() { return '2.1.99'; }
+    public function GetVersion() { return '0.2.0'; }
+    public function MinimumCMSVersion() { return '2.2.23F2'; } // for migrated permission and Jobs processing
     public function GetAuthor() { return 'Robert Campbell'; }
     public function GetAuthorEmail() { return ''; }
-    public function IsPluginModule() { return TRUE; }
     public function HasAdmin() { return TRUE; }
     public function GetAdminDescription() { return $this->Lang('moddescription'); }
     public function GetAdminSection() { return 'siteadmin'; }
-    public function LazyLoadFrontend() { return FALSE; }
-    public function LazyLoadAdmin() { return FALSE; }
-    public function VisibleToAdminUser() { return $this->CheckPermission(\CmsJobManager::MANAGE_JOBS); }
-    public function GetChangeLog() { return file_get_contents(__DIR__.'/changelog.htm'); }
+    public function IsPluginModule() { return TRUE; } // doesn't actually process tags, but this method fakes the async processing
+    public function LazyLoadFrontend() { return FALSE; } // TODO TRUE might be ok now
+    public function LazyLoadAdmin() { return FALSE; } // ditto
+    public function VisibleToAdminUser() { return $this->CheckPermission('Manage Jobs'); }
+    public function GetChangeLog() { return file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'changelog.htm'); }
     public function GetHelp() { return $this->Lang('help'); }
     public function HandlesEvents() { return TRUE; }
 
-    public function InitializeFrontend()
+    public function DoEvent($originator,$eventname,&$params)
     {
-        $this->RegisterModulePlugin();
-        $this->RestrictUnknownParams();
+        if( $originator == 'Core' && $eventname == 'ModuleUninstalled' ) {
+            $modname = trim($params['name']);
+            if( $modname ) {
+                $db = CmsApp::get_instance()->GetDb();
+                $db->Execute('DELETE FROM '.CMS_DB_PREFIX.Job::RECORDTABLE.' WHERE module=?',[$modname]);
+            }
+        }
     }
 
-    public function GetEventHelp( $name )
+    /**
+     * Get help for the OnJobFailed event
+     */
+    public function GetEventHelp($name)
     {
         return $this->Lang('evthelp_'.$name);
     }
 
-    public function GetEventDescription( $name )
+    /**
+     * Get decription of the OnJobFailed event
+     */
+    public function GetEventDescription($name)
     {
         return $this->Lang('evtdesc_'.$name);
     }
 
     /**
-     * @ignore
-     * @internal
+     * Acquire a job-processing lock
+     * This is essentially to prevent concurrent processing of jobs on
+     * threaded web-servers. The lock does not apply to any particular job
      */
-    public function get_current_job()
-    {
-        return $this->_current_job;
-    }
-
-    protected function set_current_job($job = null) // no object
-    {
-        if( !is_null($job) && !$job instanceof Job ) throw new \LogicException('Invalid data passed to '.__METHOD__);
-        $this->_current_job = $job;
-    }
-
-    protected function is_locked()
-    {
-        $this->_lock = (int) $this->GetPreference(self::LOCKPREF);
-        return ($this->_lock > 0);
-    }
-
-    protected function lock_expired()
-    {
-        $this->_lock = (int) $this->GetPreference(self::LOCKPREF);
-        if( $this->_lock && $this->_lock < time() - \CmsJobManager\utils::get_async_freq() ) return TRUE;
-        return FALSE;
-    }
-
     protected function lock()
     {
         $this->_lock = time();
         $this->SetPreference(self::LOCKPREF,$this->_lock);
     }
 
+    /**
+     * @ignore
+     */
     protected function unlock()
     {
         $this->_lock = 0;
         $this->RemovePreference(self::LOCKPREF);
     }
 
-    protected function check_for_jobs_or_tasks()
+    /**
+     * @ignore
+     */
+    protected function is_locked()
     {
-        // this is cheaper.
-        $out = \CmsJobManager\JobQueue::get_jobs(1);
-        if( $out ) return TRUE;
+        $this->_lock = (int)$this->GetPreference(self::LOCKPREF);
+        return ($this->_lock > 0);
+    }
 
-        // gotta check for tasks, which is more expensive
-        $now = time();
-        $lastcheck = (int) $this->GetPreference('tasks_lastcheck');
-        if( $lastcheck < $now - 900 ) {
-            $this->SetPreference('tasks_lastcheck',$now);
-            $res = $this->create_jobs_from_eligible_tasks();
-            if( $res ) return TRUE;
+    /**
+     * @ignore
+     */
+    protected function lock_expired()
+    {
+        $this->_lock = (int)$this->GetPreference(self::LOCKPREF);
+        if( $this->_lock > 0 ) {
+            return ($this->_lock < time() - JobOperations::get_batch_timeout());
         }
         return FALSE;
     }
 
-    protected function create_jobs_from_eligible_tasks()
-    {
-        // this creates jobs out of CmsRegularTask objects that we find,and that need to be executed.
-        $now = time();
-        $res = false;
-
-        // 1.  Get task objects from files.
-        $dir = CMS_ROOT_PATH.'/lib/tasks';
-
-        // fairly expensive as we have to iterate a directory and load files and create objects.
-        $tmp = new DirectoryIterator($dir);
-        $iterator = new RegexIterator($tmp,'/class\..+task\.php$/');
-        foreach( $iterator as $match ) {
-            $tmp = explode('.',basename($match->current()));
-            if( is_array($tmp) && count($tmp) == 4 ) {
-                $classname = $tmp[1].'Task';
-                require_once($dir.'/'.$match->current());
-                $obj = new $classname();
-                if( !$obj instanceof CmsRegularTask ) continue;
-                if( !$obj->test($now) ) continue;
-                $job = new \CMSMS\Async\RegularTask($obj);
-                $job->save();
-                $res = true;
-            }
-        }
-
-        // 2.  Get task objects from modules.
-        $opts = ModuleOperations::get_instance();
-        $modules = $opts->get_modules_with_capability('tasks');
-        if (!$modules) return false;
-        foreach( $modules as $one ) {
-            if( !is_object($one) ) $one = \cms_utils::get_module($one);
-            if( !is_object($one) ) continue; // for some reason the module exists but cannot be loaded
-            if( !method_exists($one,'get_tasks') ) continue;
-
-            $tasks = $one->get_tasks();
-            if( !$tasks ) continue;
-            if( !is_array($tasks) ) $tasks = array($tasks);
-
-            foreach( $tasks as $onetask ) {
-                if( ! is_object($onetask) ) continue;
-                if( ! $onetask instanceof CmsRegularTask ) continue;
-                if( ! $onetask->test() ) continue;
-                $job = new \CMSMS\Async\RegularTask($onetask);
-                $job->module = $one->GetName();
-                $job->save();
-                $res = true;
-            }
-        }
-
-        return $res;
-    }
-
-
-    //////////////////////////////////////////////////////////////////////////
-    // THIS STUFF SHOULD PROBABLY GO INTO A TRAIT, or at least an interface
-    //////////////////////////////////////////////////////////////////////////
-
-    public function load_job_by_id( $job_id )
-    {
-        $job_id = (int) $job_id;
-        if( $job_id < 1 ) throw new \LogicException('Invalid job_id passed to '.__METHOD__);
-
-        $db = $this->GetDb();
-        $sql = 'SELECT * FROM '.self::table_name().' WHERE id = ?';
-        $row = $db->GetRow( $sql, [ $job_id] );
-        if( !is_array($row) || !count($row) ) return null; // no object
-
-        $obj = unserialize($row['data']);
-        $obj->set_id( $row['id'] );
-        return $obj;
-    }
-
-    public function save_job(Job $job)
-    {
-        $recurs = null; //default db field-value
-        $until = 0;
-        if( \CmsJobManager\utils::job_recurs($job) ) {
-            $recurs = $job->frequency;
-            $until = $job->until;
-        }
-        $db = $this->GetDb();
-        if( !$job->id ) {
-            $sql = 'INSERT INTO '.self::table_name().' (name,created,module,errors,start,recurs,until,data) VALUES (?,?,?,?,?,?,?,?)';
-            $dbr = $db->Execute($sql,array($job->name,$job->created,$job->module,$job->errors,$job->start,$recurs,$until,serialize($job)));
-            $new_id = $db->Insert_ID();
-            $job->set_id($new_id);
-            return $new_id;
-        } else {
-            // note... we do not at any time play with the module, the data, or recus/until stuff for existing jobs.
-            $sql = 'UPDATE '.self::table_name().' SET start = ? WHERE id = ?';
-            $db->Execute($sql,array($job->start,$job->id));
-            return $job->id;
-        }
-    }
-
-    public function delete_job(Job $job)
-    {
-        if( !$job->id ) throw new \LogicException('Cannot delete a job that has no id');
-        $db = $this->GetDb();
-        $sql = 'DELETE FROM '.self::table_name().' WHERE id = ?';
-        $db->Execute($sql,array($job->id));
-    }
-
-    public function delete_jobs_by_module($module_name)
-    {
-        $module_name = trim($module_name);
-        if( !$module_name ) throw new \LogicException('Invalid module name passed to '.__METHOD__);
-
-        $db = $this->GetDb();
-        $sql = 'DELETE FROM '.self::table_name().' WHERE module = ?';
-        $db->Execute($sql,array($module_name));
-    }
-
+    /**
+     * Initiate background-jobs processing
+     */
     public function trigger_async_processing()
     {
-        // quick check to make sure this method only does something once per request
-        // and store a returnid in there for safety.
-        static $_returnid = -1;
-        if( $_returnid !== -1 ) return; // only once per request thanks.
-        $_returnid = \ContentOperations::get_instance()->GetDefaultContent();
-
-        // if this function was called because we are actually processing a cron request... stop
-        if( isset($_REQUEST['cms_cron']) ) return;
-
-        // if we triggered the thing less than N minutes ago... do nothing
+        static $_started = null;
+        // ensure this method only proceeds once per request
+        if( $_started ) {
+            return;
+        }
+        // abort if initiated during the processing of an async request
+        if( isset($_REQUEST['cms_cron']) ) {
+            return;
+        }
         $now = time();
+        // abort if initiated too soon
         $last_trigger = (int) $this->GetPreference('last_async_trigger');
-        if( $last_trigger >= $now - \CmsJobManager\utils::get_async_freq() ) return; // do nothing
-
-        $jobs = $this->check_for_jobs_or_tasks();
-        if( is_array($jobs) && !count($jobs) ) return; // nothing to do.
-
-        // this could go into a function...
-        $url_str = html_entity_decode($this->create_url('__','process',$_returnid));
-        $url_ob = new \cms_url($url_str);
-        if( !$url_ob->get_host() ) {
-            // todo: audit something.
+        if( $last_trigger >= $now - JobOperations::get_async_freq() ) {
+            return;
+        }
+        // abort if job-processing is not appropriate now (slightly expands upon the test above)
+        if( !JobOperations::check_for_jobs() ) {
             return;
         }
 
-        // gotta determine a scheme
-        $url_ob->set_queryvar('cms_cron',1);
-        $url_ob->set_queryvar('showtemplate','false');
-        $prefix_scheme = '';
-        if( !$url_ob->get_scheme() ) {
-            $url_ob->set_scheme('http');
-            if( CmsApp::get_instance()->is_https_request() ) $url_ob->set_scheme('https');
+        $_started = TRUE;
+        // initiate a frontend url cuz there will never be a logged-in user
+        // and the async handler (preprocess_mact()) looks for a frontend-specific actionid
+        $returnid = ContentOperations::get_instance()->GetDefaultContent();
+        $url_parts = parse_url($this->create_url('__','process',$returnid));
+        if( empty($url_parts['host']) ) {
+            $_started = FALSE;
+            audit('',$this->GetName(),'No host is available for async processing');
+            return;
         }
-        if( !$url_ob->get_port() ) {
-            $url_ob->set_port(80);
-            if( strtolower($url_ob->get_scheme()) == 'https' ) $url_ob->set_port(443);
-        }
-        if( strtolower($url_ob->get_scheme()) == 'https' ) $prefix_scheme = 'ssl://';
 
-        $endpoint = $url_ob->get_path();
-        $query = urldecode($url_ob->get_query());
-        if( $query ) $endpoint .= '?'.$query;
-        $post_string = $query;
-        $out = "GET ".$endpoint." HTTP/1.1\r\n";
-        $out .= 'Host: '.$url_ob->get_host()."\r\n";
-        $out .= "Connection: Close\r\n\r\n";  // two lines
+        $secure = (!empty($url_parts['scheme'])) ?
+            (strcasecmp($url_parts['scheme'],'https') == 0) :
+            CmsApp::get_instance()->is_https_request();
 
-        $this->SetPreference('last_async_trigger',$now+1);
-
-        try {
-            $fp = @fsockopen($prefix_scheme.$url_ob->get_host(),$url_ob->get_port(),$errno,$errstr,1.0); // fsockopen involves PHP header(s)
-            if( !$fp ) {
-                $msg = 'Could not connect to the async processing action';
-                if( $errstr ) $msg .= ': '.$errstr;
-                throw new \RuntimeException($msg);
+        $transport = 'tcp'; //default, will be treated as insecure
+        if( $secure ) {
+            $opts = stream_get_transports();
+            if( in_array('tls',$opts) ) {
+                $transport = 'tls';
             }
-            fwrite($fp,$out);
-            $data = fgets($fp);
-            $code = (int) substr($data,9,3);
-            fclose($fp);
-            if( $code != 200 ) {
-                audit('',$this->GetName(),'Received '.$code.' response when trying to trigger async processing');
+            elseif( in_array('ssl',$opts) ) { // try sslv2 and sslv3
+                $transport = 'ssl';
             }
         }
-        catch( \Exception $e ) {
-            debug_to_log('exception '.$e->GetMessage());
+
+        if( empty($url_parts['port']) ) {
+            $url_parts['port'] = ($secure) ? 443 : 80; // TODO 443 might be wrong for this site
+        }
+
+        $host = $url_parts['host'];
+        $clean = str_replace('&amp;','&',$url_parts['query']);
+        $endpoint = $url_parts['path'].'?'.$clean.'&cms_cron=1'; // former &showtemplate=false seems bad for async-request processing
+        $req = "GET $endpoint HTTP/1.1\r\nHost: $host\r\nConnection: Close\r\n\r\n";
+
+        // c.f. phpmailer::getSMTPConnection() which prefers stream_socket_client with fsockopen fallback
+        // stream_socket_client() allows more flexibility but allegedly,
+        // some sitehosts disable that function
+        if( function_exists('stream_socket_client') ) {
+            $remote = $transport.'://'.$host.':'.$url_parts['port'];
+
+            if( $transport == 'tcp' ) {
+                $context = stream_context_create();
+            }
+            else {
+                //internal-use only, skip verification
+                $opts = [
+                    $transport => [
+                        'allow_self_signed' => TRUE,
+                        'verify_host' => FALSE,
+                        'verify_peer' => FALSE
+                    ]
+                ];
+                $context = stream_context_create($opts);
+            }
+
+            $this->SetPreference('last_async_trigger',$now + 1);
+            try {
+                $fp = stream_socket_client($remote,$errno,$errstr,1,STREAM_CLIENT_ASYNC_CONNECT,$context);
+                if( !$fp ) {
+                    $msg = 'Could not connect to the async processing action';
+                    if( $errstr ) $msg .= ': '.$errstr;
+                    throw new RuntimeException($msg);
+                }
+                fwrite($fp,$req);
+                stream_socket_shutdown($fp,STREAM_SHUT_RDWR);
+            }
+            catch( Exception $e ) {
+                debug_to_log('async connection exception '.$e->GetMessage());
+            }
+        }
+        else {
+            // try to connect using fsockopen()
+            // bombs on a secure site without an effective certificate
+            $remote = $transport.'://'.$host;
+
+            $this->SetPreference('last_async_trigger',$now + 1);
+            try {
+                $fp = @fsockopen($remote,$url_parts['port'],$errno,$errstr,1.0); // fsockopen involves PHP header(s)
+                if( !$fp ) {
+                    $msg = 'Could not connect to the async processing action';
+                    if( $errstr ) $msg .= ': '.$errstr;
+                    throw new RuntimeException($msg);
+                }
+                stream_set_blocking($fp,FALSE);
+                stream_set_timeout($fp,2);
+                fwrite($fp,$req);
+                fclose($fp);
+            }
+            catch( Exception $e ) {
+                debug_to_log('async connection exception '.$e->GetMessage());
+            }
         }
     }
-
-} // class CmsJobManager
+} // class
