39404978441c49737c252eaef4d4c8d5872188b3
tom2tom <tpgww@onepost.net>
2026-04-08 16:09:43 +1000

    use renamed class ProfilesCache add job

--- modules/FilePicker/FilePicker.module.php
+++ modules/FilePicker/FilePicker.module.php
@@ -4,7 +4,6 @@
 # Module: FilePicker - a CMSMS addon module to provide file picking capabilities.
 # (c) 2016 Fernando Morgado <jomorg@cmsmadesimple.org>
 # (c) 2016 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
-#
 #-------------------------------------------------------------------------
 # This file is part of FilePicker
 # FilePicker is free software; you can redistribute it and/or modify
@@ -24,12 +23,13 @@
 # END_LICENSE
 
 use CMSMS\FilePickerInterface;
-//use CMSMS\FilePickerProfile;
+use CMSMS\FilePickerProfile;
 use CMSMS\FileType;
 use CMSMS\FileTypeHelper;
+use CMSMS\HookManager;
 use FilePicker\Profile;
 use FilePicker\ProfileDAO;
-use FilePicker\TemporaryProfileStorage;
+use FilePicker\ProfilesCache;
 
 require_once cms_join_path(__DIR__, 'lib', 'class.ProfileDAO.php');
 
@@ -43,7 +43,7 @@ final class FilePicker extends CMSModule implements FilePickerInterface
     public function __construct()
     {
         parent::__construct();
-        $this->_dao = new ProfileDAO( $this );
+        $this->_dao = new ProfileDAO($this);
         $this->_typehelper = new FileTypeHelper();
     }
 
@@ -55,11 +55,6 @@ final class FilePicker extends CMSModule implements FilePickerInterface
     private function _decodefilename($encodedfilename)
     {
         return base64_decode($encodedfilename . '==');
-    }
-
-    public function VisibleToAdminUser()
-    {
-        return $this->CheckPermission('Modify Site Preferences');
     }
 
     private function _GetTemplateObject()
@@ -74,14 +69,16 @@ final class FilePicker extends CMSModule implements FilePickerInterface
     public function GetDependencies() { return array('FileManager' => '0.4'); }
     public function GetFriendlyName() { return $this->Lang('friendlyname'); }
     public function GetHelp() { return $this->Lang('help'); }
-    public function GetVersion() { return '1.0.8'; }
+    public function GetVersion() { return '1.1'; }
     public function HasAdmin() { return TRUE; }
     public function MinimumCMSVersion() { return '2.2'; }
+    public function VisibleToAdminUser() { return $this->CheckPermission('Modify Site Preferences'); }
 
     public function HasCapability( $capability, $params = array() )
     {
         switch( $capability ) {
-            case 'contentblocks': // aka CmsCoreCapabilities::CONTENT_BLOCKS
+            case CmsCoreCapabilities::CONTENT_BLOCKS:
+            case CmsCoreCapabilities::TASKS:
             case 'filepicker':
             case 'upload':
                 return TRUE;
@@ -92,41 +89,68 @@ final class FilePicker extends CMSModule implements FilePickerInterface
 
     public function GetAdminMenuItems()
     {
-        $out = array();
         if( $this->VisibleToAdminUser() ) {
-            $out[] = CmsAdminMenuItem::from_module($this);
+            $obj = CmsAdminMenuItem::from_module($this);
+            $obj->section = 'siteadmin';
+            $obj->title = $this->Lang('title_filepicker_settings');
+            $obj->description = $this->Lang('desc_filepicker_settings');
+            return [$obj];
         }
-        return $out;
+        return [];
     }
 
-    //{content_module} tag setup
-    public function GetContentBlockFieldInput($blockName, $value, $params, $adding, ContentBase $content_obj)
+    public function get_tasks()
+    {
+        $fp = cms_join_path(__DIR__,'lib','class.ResetCounterJob.php');
+        if( is_file($fp) ) {
+            if( func_num_args() > 0 && func_get_arg(0) ) { // want filepath(s)
+                return [$fp];
+            } else {
+                require_once $fp;
+                return [new FilePicker\ResetCounterJob()];
+            }
+        }
+        return [];
+    }
+
+    /**
+     *{content_module} tag setup
+     *
+     * @param string $blockName
+     * @param string $value
+     * @param array $params
+     * @param bool $adding UNUSED
+     * @param ContentBase $content_obj UNUSED
+     * @return string
+     */
+    public function GetContentBlockFieldInput($blockName, $value, array $params, $adding, ContentBase $content_obj)
     {
         if( !$blockName ) return '';
-        // workaround $adding not set in ContentManager v1.0 action admin_editcontent
-        if (!$adding && version_compare(CMS_VERSION, '2.1') < 0 && $content_obj->Id() == 0) {
-            $adding = TRUE; //TODO no profile-relevance
-        }
         $uid = get_userid(FALSE);
         $profile_name = get_parameter_value($params, 'profile');
         $profile = $this->get_profile_or_default($profile_name, '', $uid);
         if( $params ) {
-            unset($params['profile']);
-            unset($params['top']); // no top-folder change allowed here
-/* TODO any other relevant limitations? e.g.
-            if( !$this->CheckPermission('Modify Files') ) {
-                $params['can_upload'] = FilePickerProfile::FLAG_NONE;
-                $params['can_delete'] = FilePickerProfile::FLAG_NONE;
-                $params['can_mkdir'] = FilePickerProfile::FLAG_NONE;
-            }
-*/
-            $profile = $profile->overrideWith($params);
+            unset($params['profile'],
+                $params['top'], // no top-folder change allowed here
+                $params['can_delete'],// no writability-change allowed here
+                $params['can_mkdir'],
+                $params['can_upload']);
+            // TODO any other relevant property-additions or -limitations?
+            $profile->overrideWith($params);
         }
         return $this->get_html($blockName, $value, $profile);
     }
 
-    //{content_module} tag retriever
-    public function GetContentBlockFieldValue($blockName, $blockParams, $inputParams, ContentBase $content_obj)
+    /**
+     * {content_module} tag retriever
+     *
+     * @param string $blockName
+     * @param array $blockParams UNUSED
+     * @param array $inputParams
+     * @param ContentBase $content_obj UNUSED
+     * @return string maybe empty
+     */
+    public function GetContentBlockFieldValue($blockName, array $blockParams, $inputParams, ContentBase $content_obj)
     {
         if( $blockName && isset($inputParams[$blockName]) ) {
             return $inputParams[$blockName];
@@ -134,26 +158,42 @@ final class FilePicker extends CMSModule implements FilePickerInterface
         return ''; // should never happen
     }
 
-    //{content_module} tag validator
-    public function ValidateContentBlockFieldValue($blockName, $value, $blockparams, ContentBase $content_obj)
+    /**
+     * {content_module} tag validator
+     *
+     * @param string $blockName
+     * @param string $value url
+     * @param array $blockparams
+     * @param ContentBase $content_obj UNUSED
+     * @return string empty or error-message
+     */
+    public function ValidateContentBlockFieldValue($blockName, $value, array $blockparams, ContentBase $content_obj)
     {
-        $value = trim($value);
+        $value = trim((string)$value);
         if( $value ) {
             $res = cms_utils::validate_url($value, '!' . FileType::TYPE_EXECUTABLE);
             if( $res !== TRUE ) return $res;
         }
         elseif( isset($blockparams['required']) ) {
-            return $this->lang('error_missing_value', $blockName);
+            return $this->lang('err_missing_value', $blockName);
         }
         return '';
     }
 
-    //{content_module} tag renderer
-    // If $blockparams includes a member 'format', then values
-    // 'absolute' 'anchor' and 'image' may be specified for that parameter
-    public function RenderContentBlockField($blockName, $value, $blockparams, ContentBase $content_obj)
+    /**
+     * {content_module} tag renderer
+     * If $blockparams includes a member 'format', then values
+     * 'absolute' 'anchor' and 'image' may be specified for that parameter
+     *
+     * @param string $blockName
+     * @param string $value
+     * @param array $blockparams
+     * @param ContentBase $content_obj UNUSED
+     * @return string maybe empty or $value
+     */
+    public function RenderContentBlockField($blockName, $value, array $blockparams, ContentBase $content_obj)
     {
-        $value = trim($value);
+        $value = trim((string)$value);
         if( $value ) {
             if( !empty($blockparams['format']) ) {
                 switch ($blockparams['format']) {
@@ -175,52 +215,97 @@ final class FilePicker extends CMSModule implements FilePickerInterface
         return '';
     }
 
+    /**
+     *
+     * @param string $path Default '' hence filemanager_utils::get_cwd()
+     * @return array
+     */
     public function GetFileList($path = '')
     {
-        return filemanager_utils::get_file_list($path);
+        $sortby = 'nameasc'; //TODO 'none' if relevant profile has no sorting
+        return filemanager_utils::get_file_list($path, $sortby);
     }
 
+    /**
+     *
+     * @param string $profile_name
+     * @param string $dir Directory path Default ''
+     * @param int $uid Current user numeric id or < 0 for frontend use Default 0
+     * @return Profile
+     */
     public function get_profile_or_default($profile_name, $dir = '', $uid = 0)
     {
         $profile_name = trim((string)$profile_name);
-        $profile = ( $profile_name ) ? $this->_dao->loadByName( $profile_name ) : null;
-        if( !$profile ) $profile = $this->get_default_profile( $dir, $uid );
+        $profile = ($profile_name) ? $this->_dao->loadByName($profile_name) : null;
+        if ($profile) {
+            if ($uid < 1 || !check_permission($uid,'Modify Files')) {
+                $profile->overrideWith([
+                'can_delete' => FilePickerProfile::FLAG_NONE,
+                'can_mkdir' => FilePickerProfile::FLAG_NONE,
+                'can_upload' => FilePickerProfile::FLAG_NONE
+                ]);
+            }
+        } else {
+            $profile = $this->get_default_profile($dir, $uid);
+        }
         return $profile;
     }
 
+    /**
+     *
+     * @param string $dir Directory path Default ''
+     * @param int $uid  Current user numeric id or < 0 for frontend use Default 0
+     * @return Profile
+     */
     public function get_default_profile($dir = '', $uid = 0)
     {
-        /* $dir is absolute */
-        $profile = $this->_dao->loadDefault();
-        if( $profile ) return $profile;
-
-        $profile = new Profile();
+        // TODO try for a specific default for dir+uid
+        $profile = $this->_dao->loadDefault(); // generic default if any
+        if (!$profile) { $profile =  new Profile(); }
+        if ($uid < 1 || !check_permission($uid,'Modify Files')) {
+            $profile->overrideWith([
+            'can_delete' => FilePickerProfile::FLAG_NONE,
+            'can_mkdir' => FilePickerProfile::FLAG_NONE,
+            'can_upload' => FilePickerProfile::FLAG_NONE
+            ]);
+        }
         return $profile;
     }
 
+    /**
+     *
+     * @return string
+     */
     public function get_browser_url()
     {
         return $this->create_url('m1_', 'filepicker');
     }
 
-    //TODO support optional tailored useprefix variable for template & backend js, without clobbering $profile
+    /**
+     *
+     * @staticvar bool $scripted
+     * @param string $name
+     * @param string $value
+     * @param Profile $profile
+     * @param bool $required
+     * @return mixed string | null
+     */
     public function get_html($name, $value, Profile $profile, $required = FALSE)
     {
+        static $scripted = false;
+        if( !$scripted ) { // once-per-request
+            HookManager::add_hook('admin_add_headtext', function() {
+                $root_url = CMS_ROOT_URL;
+                return "<script src=\"$root_url/lib/jquery/js/jquery.cmsms_filepicker.js\" defer></script>\n";
+            });
+            $scripted = true;
+        }
+
         $_instance = 'i'.uniqid();
         if( $value === '-1' ) $value = '';
 
-        // store the profile as a 'useonce' and add its signature to the params on the url
-        $sig = TemporaryProfileStorage::set( $profile );
-        $smarty = cmsms()->GetSmarty(); // OR $this->_GetTemplateObject() ?
-        $modname = $this->GetName();
-        $tpl = $smarty->createTemplate("module_file_tpl:$modname;contentblock.tpl", null, $modname, $smarty);
-        $tpl->assign('mod', $this);
-        $tpl->assign('sig', $sig);
-        $tpl->assign('blockName', $name);
-        $tpl->assign('value', $value);
-        $tpl->assign('instance', $_instance);
-        $tpl->assign('profile', $profile);
-        $tpl->assign('required', $required);
+        $sig = ProfilesCache::get_instance()->set($profile);
+
         switch( $profile->type ) {
         case FileType::TYPE_IMAGE:
             $key = 'select_an_image';
@@ -248,11 +333,27 @@ final class FilePicker extends CMSModule implements FilePickerInterface
             $key = 'select_a_file';
             break;
         }
-        $tpl->assign('title', $this->Lang($key));
+
+        $smarty = cmsms()->GetSmarty(); // OR $this->_GetTemplateObject() ?
+        $modname = $this->GetName();
+        $tpl = $smarty->createTemplate("module_file_tpl:$modname;contentblock.tpl", null, $modname, $smarty);
+        $tpl->assign('mod', $this)
+         ->assign('sig', $sig)
+         ->assign('blockName', $name)
+         ->assign('value', $value)
+         ->assign('instance', $_instance)
+         ->assign('profile', $profile)
+         ->assign('required', $required)
+         ->assign('title', $this->Lang($key));
         return $tpl->fetch();
     }
 
-    // INTERNAL UTILITY FUNCTION
+    /**
+     * Report whether the supplied filepath is for an image
+     *
+     * @param string $fullpath
+     * @return bool
+     */
     public function is_image($fullpath)
     {
         $fullpath = trim((string)$fullpath);
@@ -261,7 +362,14 @@ final class FilePicker extends CMSModule implements FilePickerInterface
         return $this->_typehelper->is_image($fullpath);
     }
 
-    // INTERNAL UTILITY FUNCTION
+    /**
+     * Report whether the supplied filepath is acceptable in terms of the
+     * supplied Profile properties
+     *
+     * @param Profile $profile
+     * @param string $fullpath
+     * @return bool
+     */
     public function is_acceptable_filename(Profile $profile, $fullpath)
     {
         $fullpath = trim((string)$fullpath);
@@ -272,30 +380,50 @@ final class FilePicker extends CMSModule implements FilePickerInterface
         if( !$profile->show_hidden ) {
             if( filemanager_utils::is_hidden_file($fullpath) ) { return FALSE; }
         }
-        if( $profile->match_prefix && !startswith($basename, $profile->match_prefix) ) { return FALSE; }
-        if( $profile->exclude_prefix && startswith($basename, $profile->exclude_prefix) ) { return FALSE; }
+
+        if( !isset($this->winos) ) {
+            if( function_exists('php_uname') && ($tmp = php_uname('s')) ) { //might return null (undocumented)
+                $this->winos = stripos($tmp,'windo') !== false;// running on some flavour of Windows
+            }
+            else {
+                $this->winos = (PATH_SEPARATOR == ';');
+            }
+        }
+        $flags = FNM_PATHNAME | FNM_PERIOD;
+        if( $this->winos ) { $flags |= FNM_CASEFOLD; }
+
+        if( $profile->exclude_prefix && fnmatch($profile->exclude_prefix.'*', $basename, $flags) ) {
+            return FALSE;
+        }
 
         switch( $profile->type ) {
         case FileType::TYPE_IMAGE:
-            return $this->_typehelper->is_image($fullpath);
+            if( !$this->_typehelper->is_image($fullpath) ) { return FALSE; }
+            break;
 
         case FileType::TYPE_AUDIO:
-            return $this->_typehelper->is_audio($fullpath);
+            if( !$this->_typehelper->is_audio($fullpath) ) { return FALSE; }
+            break;
 
         case FileType::TYPE_VIDEO:
-            return $this->_typehelper->is_video($fullpath);
+            if( !$this->_typehelper->is_video($fullpath) ) { return FALSE; }
+            break;
 
         case FileType::TYPE_MEDIA:
-            return $this->_typehelper->is_media($fullpath);
+            if( !$this->_typehelper->is_media($fullpath) ) { return FALSE; }
+            break;
 
         case FileType::TYPE_XML:
-            return $this->_typehelper->is_xml($fullpath);
+            if( !$this->_typehelper->is_xml($fullpath) ) { return FALSE; }
+            break;
 
         case FileType::TYPE_DOCUMENT:
-            return $this->_typehelper->is_document($fullpath);
+            if( !$this->_typehelper->is_document($fullpath) ) { return FALSE; }
+            break;
 
         case FileType::TYPE_ARCHIVE:
-            return $this->_typehelper->is_archive($fullpath);
+            if( !$this->_typehelper->is_archive($fullpath) ) { return FALSE; }
+            break;
 
         default:
             if( $this->_typehelper->is_executable($fullpath) ) {
@@ -305,6 +433,10 @@ final class FilePicker extends CMSModule implements FilePickerInterface
                 }
             }
         }
-        return TRUE; // passed
+
+        if( $profile->match_prefix && !fnmatch($profile->match_prefix.'*', $basename, $flags) ) {
+            return FALSE;
+        }
+        return TRUE;
     }
 } // class
