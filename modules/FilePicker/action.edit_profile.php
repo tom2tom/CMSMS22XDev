4253ba3e96613f25ae4f944a83c12a58ad543b93
tom2tom <tpgww@onepost.net>
2026-04-12 17:26:42 +1000

    use mutable profiles

--- modules/FilePicker/action.edit_profile.php
+++ modules/FilePicker/action.edit_profile.php
@@ -4,7 +4,6 @@
 # Module FilePicker action
 # (c) 2016 Fernando Morgado <jomorg@cmsmadesimple.org>
 # (c) 2016 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
-#
 #-------------------------------------------------------------------------
 # This file is part of FilePicker
 # FilePicker is free software; you can redistribute it and/or modify
@@ -17,13 +16,14 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
+# along with this program. If not, read the license online at
+# https://www.gnu.org/licenses/old-licenses/gpl2.0.html
 #-------------------------------------------------------------------------
 # END_LICENSE
 
+use CMSMS\FileType;
 use FilePicker\Profile;
+
 if( !defined('CMS_VERSION') ) exit;
 if( !$this->VisibleToAdminUser() ) exit;
 
@@ -31,34 +31,76 @@ if( isset($params['cancel']) ) $this->RedirectToAdminTab();
 
 try {
     $profile_id = get_parameter_value($params,'pid',0);
-    $profile = new Profile();
-
     if( $profile_id > 0 ) {
-        $profile = $this->_dao->loadById( $profile_id );
-        if( !$profile ) throw new \LogicException('Invalid profile id passed to edit_profile action');
+        $profile = $this->_dao->loadById($profile_id);
+        if( !$profile ) throw new LogicException('Invalid profile id passed to edit_profile action');
     }
+    else {
+        $profile = new Profile();
+    }
+
+    $clone = !empty($params['clone']);
 
     if( isset($params['submit']) ) {
+        $pparms = array_diff_key($params,['action'=>1,'clone'=>1,'default'=>1,'pid'=>1,'submit'=>1]);
+        //TODO some of these might be better as FilePickerProfile::FLAG_* int values
+        foreach( ['show_thumbs','show_hidden','can_upload','can_delete','can_mkdir','sort'] as $boolopt ) {
+            if( !isset($params[$boolopt]) ) { $pparms[$boolopt] = 0; }
+        }
+        if( $clone ) {
+            $now = time();
+            $pparms = ['id' => 0, 'created' => $now, 'modified'=> $now] + $pparms; // clear its id property
+        }
         try {
-            $profile = $profile->overrideWith( $params );
-            $this->_dao->save( $profile );
+            $profile->overrideWith($pparms);
+            $profile->validate();
+            $profile = $this->_dao->save($profile);
+            if( !empty($params['default']) ) {
+                $this->_dao->setDefault($profile);
+            }
+            elseif( $profile->id == $this->_dao->getDefaultProfileId() ) {
+                $this->_dao->clearDefault();
+            }
             $this->RedirectToAdminTab();
         }
-        catch( \FilePicker\ProfileException $e ) {
-            echo $this->ShowErrors($this->Lang($e->GetMessage()));
+        catch( FilePicker\ProfileException $e ) {
+            $this->ShowErrors($this->Lang($e->GetMessage()));
         }
     }
 
-    $modname=$this->GetName();
+    $dflt_profile_id = $this->_dao->getDefaultProfileId();
+    $default = ($dflt_profile_id > 0 && $dflt_profile_id == $profile->id);
+
+    if( $clone ) {
+        $profile->overrideWith(['name'=>$this->Lang('copytemplate',$profile->name)]);
+    }
+
+    $choices = [
+    //TODO langify these
+    FileType::TYPE_IMAGE => 'Images',
+    FileType::TYPE_AUDIO => 'Audio files',
+    FileType::TYPE_VIDEO => 'Video files',
+    FileType::TYPE_MEDIA => 'Media files',
+    FileType::TYPE_XML   => 'XML files',
+    FileType::TYPE_DOCUMENT => 'Documents',
+    FileType::TYPE_ARCHIVE => 'Archives',
+//  FileType::TYPE_EXECUTABLE => 'Executable files', TODO allow if some kind of 'advanced' ?
+    FileType::TYPE_ANY => 'All types'
+    ];
+
+    $modname = $this->GetName();
     $tpl = $smarty->createTemplate("module_file_tpl:$modname;edit_profile.tpl",null,$modname,$smarty);
+    $tpl->assign('doclone',$clone);
     $tpl->assign('profile',$profile);
+    $tpl->assign('default',$default);
+    $tpl->assign('filetype_opts',$choices);
     $tpl->display();
 }
-catch( \CmsInvalidDataException $e ) {
+catch( CmsInvalidDataException $e ) {
     $this->SetError( $this->Lang( $e->GetMessage() ) );
     $this->RedirectToAdminTab();
 }
-catch( \Exception $e ) {
+catch( Exception $e ) {
     $this->SetError( $e->GetMessage() );
     $this->RedirectToAdminTab();
 }
