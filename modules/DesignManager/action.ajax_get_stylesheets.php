cc8257da68f15480792d084ae9f41dcfa1ca7bed
tom2tom <tpgww@onepost.net>
2026-04-12 18:40:08 +1000

    various housekeeping

--- modules/DesignManager/action.ajax_get_stylesheets.php
+++ modules/DesignManager/action.ajax_get_stylesheets.php
@@ -5,14 +5,13 @@ for ($cnt = 0; $cnt < $num; $cnt++) { ob_end_clean(); }
 
 try {
     if( !$this->CheckPermission('Manage Stylesheets') ) throw new Exception($this->Lang('error_permission'));
-    $tmp = get_parameter_value($_REQUEST,'filter');
+    $tmp = get_parameter_value($_REQUEST,'filter'); //TODO something dynamic from downstream, not just defaults
     if( !$tmp ) throw new Exception($this->Lang('error_missingparam'));
+    $filter = json_decode($tmp,true);
+    if( !$filter ) throw new Exception($this->Lang('error_missingparam'));
 
-    $userid = get_userid();
     $modname = $this->GetName();
     $tpl = $smarty->createTemplate("module_file_tpl:$modname;ajax_get_stylesheets.tpl",null,$modname,$smarty);
-
-    $filter = json_decode($tmp,TRUE);
 
     $designchoices = [];
     $designs = CmsLayoutCollection::get_all();
@@ -27,9 +26,11 @@ try {
         asort($designchoices);
     }
     asort($filter);
+
     $tpl->assign('css_filter',$filter);
     $tpl->assign('design_names',$designchoices);
 
+    $userid = get_userid();
     $locks = CmsLockOperations::get_locks('stylesheet',0,$userid); //lock(s) held by other users
     $have_locks = $locks && is_array($locks);
     $locks = CmsLockOperations::get_locks('stylesheet',$userid); //lock(s) held by current user
@@ -47,22 +48,25 @@ try {
         $self_locks = false;
     }
 
-    $css_query = new CmsLayoutStylesheetQuery($filter);
-    $csslist = $css_query->GetMatches();
-    $tpl->assign('stylesheets',$csslist);
     $css_nav = [];
-    $css_nav['pagelimit'] = $css_query->limit;
-    $css_nav['numpages'] = $css_query->numpages;
-    $css_nav['numrows'] = $css_query->totalrows;
-    $css_nav['curpage'] = (int)($css_query->offset / $css_query->limit) + 1;
-    $tpl->assign('css_nav',$css_nav);
-    $tpl->assign('manage_designs',$this->CheckPermission('Manage Designs'));
+    $css_query = new CmsLayoutStylesheetQuery($filter); //TODO dynamic params from downstream, not just defaults
+    $sheets = $css_query->GetMatches();
+    if( $sheets ) {
+        $tpl->assign('stylesheets',$sheets,true);
+//        if( $css_query->numpages > 1 ) {
+            $css_nav['pagelimit'] = $css_query->limit;
+            $css_nav['numpages'] = $css_query->numpages;
+            $css_nav['numrows'] = $css_query->totalrows;
+            $css_nav['curpage'] = (int)($css_query->offset / $css_query->limit) + 1;
+//        }
+    }
+    $tpl->assign('css_nav',$css_nav,true);
     $tpl->assign('have_css_locks',$have_locks);
     $tpl->assign('have_css_selflocks',$self_locks);
     if( $self_locks ) { $tpl->assign('which_selflocks',$itemids); }
-    if( $have_locks ) { $tpl->assign('iconsteal_url',$this->GetModuleURLPath().'/icons/steal.png'); }
+    if( $have_locks ) { $tpl->assign('iconsteal_url',$this->GetModuleURLPath().'/images/steal.png'); }
     $tpl->assign('lock_timeout',$this->GetPreference('lock_timeout'));
-//  $tpl->assign('has_add_right',$this->CheckPermission('X') || $this->CheckPermission('Y'));
+//  $tpl->assign('has_add_right',$this->CheckPermission(''Manage Stylesheets') || $this->CheckPermission('TODO'));
     $tpl->assign('userid',$userid);
     $tpl->display();
 }
--- modules/DesignManager/action.ajax_get_templates.php
+++ modules/DesignManager/action.ajax_get_templates.php
@@ -1,28 +1,34 @@
 <?php
+// this action processes templates-data and also designs-data (for use in the templates-filter)
 
 $num = count(ob_list_handlers());
 for( $cnt = 0; $cnt < $num; $cnt++ ) { ob_end_clean(); }
 
-$userid = get_userid();
-$modname = $this->GetName();
-$tpl = $smarty->createTemplate("module_file_tpl:$modname;ajax_get_templates.tpl",null,$modname,$smarty);
-
 try {
-    $tmp = get_parameter_value($_REQUEST,'filter');
-    $filter = json_decode($tmp,TRUE);
-    if( !$this->CheckPermission('Modify Templates') ) $filter[] = 'e:'.$userid;
+    $tmp = get_parameter_value($_REQUEST,'filter'); //TODO something dynamic from downstream, not just defaults
+    if( !$tmp ) throw new Exception($this->Lang('error_missingparam'));
+    $filter = json_decode($tmp,true);
+    if( !$filter ) throw new Exception($this->Lang('error_missingparam'));
 
-    $tpl_query = new CmsLayoutTemplateQuery($filter);
+    $userid = get_userid();
+    if( !check_permission($userid,'Modify Templates') ) $filter[] = 'e:'.$userid;
+
+    $modname = $this->GetName();
+    $tpl = $smarty->createTemplate("module_file_tpl:$modname;ajax_get_templates.tpl",null,$modname,$smarty);
+
+    $tpl_nav = [];
+    $tpl_query = new CmsLayoutTemplateQuery($filter); //TODO dynamic params from downstream, not just defaults
     $templates = $tpl_query->GetMatches();
     if( $templates ) {
-        $tpl->assign('templates',$templates);
-        $tpl_nav = [];
-        $tpl_nav['pagelimit'] = $tpl_query->limit;
-        $tpl_nav['numpages'] = $tpl_query->numpages;
-        $tpl_nav['numrows'] = $tpl_query->totalrows;
-        $tpl_nav['curpage'] = (int)($tpl_query->offset / $tpl_query->limit) + 1;
-        $tpl->assign('tpl_nav',$tpl_nav);
+        $tpl->assign('templates',$templates,true);
+        if( $tpl_query->numpages > 1 ) {
+            $tpl_nav['pagelimit'] = $tpl_query->limit;
+            $tpl_nav['numpages'] = $tpl_query->numpages;
+            $tpl_nav['numrows'] = $tpl_query->totalrows;
+            $tpl_nav['curpage'] = (int)($tpl_query->offset / $tpl_query->limit) + 1;
+        }
     }
+    $tpl->assign('tpl_nav',$tpl_nav,true);
 
     $designchoices = [];
     $designs = CmsLayoutCollection::get_all();
@@ -49,6 +55,7 @@ try {
             $publictypes[$tid] = $types[$i]->get_langified_display_value();
             $typechoices[$tid] = $types[$i];
         }
+        //TODO reconcile sorting with filter-sorting params, if any
         asort($publictypes);
         $tpl->assign('list_types',$publictypes);
 //      asort($typechoices);
@@ -77,13 +84,12 @@ try {
     }
 
     $tpl->assign('have_tpl_locks',$have_locks);
-    if( $have_locks ) { $tpl->assign('iconsteal_url',$this->GetModuleURLPath().'/icons/steal.png'); }
+    if( $have_locks ) { $tpl->assign('iconsteal_url',$this->GetModuleURLPath().'/images/steal.png'); }
     $tpl->assign('have_tpl_selflocks',$self_locks);
     if( $self_locks ) { $tpl->assign('which_selflocks',$itemids); }
     $tpl->assign('lock_timeout',$this->GetPreference('lock_timeout'));
     $tpl->assign('coretypename',CmsLayoutTemplateType::CORE);
     $tpl->assign('manage_templates',$this->CheckPermission('Modify Templates'));
-    $tpl->assign('manage_designs',$this->CheckPermission('Manage Designs'));
     $tpl->assign('has_add_right',
         $this->CheckPermission('Modify Templates') ||
         $this->CheckPermission('Add Templates'));
