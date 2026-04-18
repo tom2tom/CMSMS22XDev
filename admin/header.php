1bf0f7ce8e50f40cad070d285f5d9fd99b9bbeed
tom2tom <tpgww@onepost.net>
2026-04-18 17:50:01 +1000

    by default, enable Smarty caching

--- admin/header.php
+++ admin/header.php
@@ -13,18 +13,23 @@ if (isset($USE_THEME) && $USE_THEME == false) {
 else {
     debug_buffer('before theme load');
     $themeObject = cms_utils::get_theme_object();
-    $smarty->assign('secureparam', CMS_SECURE_PARAM_NAME . '=' . $_SESSION[CMS_USER_KEY]);
     debug_buffer('after theme load');
+    $smarty->assign('secureparam',CMS_SECURE_PARAM_NAME . '=' . $_SESSION[CMS_USER_KEY],true); //also in adminthemebase for some requests
 
     // Display notification stuff from modules
     // should be controlled by preferences or something
-    $ignoredmodules = explode(',',cms_userprefs::get_for_user($userid,'ignoredmodules'));
-    if( cms_siteprefs::get('enablenotifications',1) && cms_userprefs::get_for_user($userid,'enablenotifications',1) ) {
-        // Display a warning sitedownwarning
-        $sitedown_message = lang('sitedownwarning', TMP_CACHE_LOCATION . '/SITEDOWN');
-        $sitedown_file = TMP_CACHE_LOCATION . '/SITEDOWN';
-        if (file_exists($sitedown_file)) $themeObject->AddNotification(1,'Core',$sitedown_message);
-    }
+//  $ignoredmodules = explode(',',cms_userprefs::get_for_user($userid,'ignoredmodules')); no such thing
 
+    $sitedown_file = TMP_CACHE_LOCATION . DIRECTORY_SEPARATOR . 'SITEDOWN';
+    if (file_exists($sitedown_file)) {
+        if (cms_siteprefs::get('enablenotifications',true) && cms_userprefs::get_for_user($userid,'enablenotifications',true)) {
+            // Display a warning
+            $sitedown_message = lang('sitedownwarning',$sitedown_file);
+            $themeObject->AddNotification(1,'Core',$sitedown_message);
+        }
+    }
+    else {
+        $smarty->changeCaching(true); // by default, non-module admin pages support caching
+    }
     $themeObject->do_header();
 }
