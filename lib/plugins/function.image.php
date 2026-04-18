929f8d516e0f99931b11030b85ea31078c26fb7e
tom2tom <tpgww@onepost.net>
2026-04-18 17:28:05 +1000

    housekeeping

--- lib/plugins/function.image.php
+++ lib/plugins/function.image.php
@@ -15,55 +15,57 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-// THIS PLUGIN DEPRECATED SINCE CMSMS 2.2 INSTEAD USE {page_image ...}
-
-function smarty_function_image($params, $smarty)
+function smarty_function_image($params, $template)
 {
-  $gCms = CmsApp::get_instance();
-  $text = '';
-  $imgstart = '<img src=';
-  $imgend = '>';
-  if( !empty($params['src'] ) ) {
-      $text = $imgstart .= '"'.$gCms->config['image_uploads_url'].'/'.$params['src'].'"'; //TODO possible subdir, one of cms_siteprefs::get('content * _path')) ?
-      $size = @getimagesize($gCms->config['image_uploads_path'].'/'.$params['src']);
+	if( !empty($params['src']) ) {
+		$config = cms_config::get_instance();
+		$relurl = strtr(trim($params['src'],' \\/'),'\\','/'); // possibly a subdir e.g. one of cms_siteprefs::get('content * _path')
+		$text = '<img src="'.$config['image_uploads_url'].'/'.$relurl.'"';
+		$fullpath = $config['image_uploads_path'].DIRECTORY_SEPARATOR.strtr($relurl,'\\/',DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR);
 
-      if( !empty($params['width'] ) ) {
-          $text .= ' width="'.$params['width'].'"';
-      } elseif ($size[0] > 0) {
-          $text .= ' width="'.$size[0].'"';
-      }
+		if( !empty($params['width']) ) {
+			$text .= ' width="'.trim($params['width'],' "\'').'"';
+		} else {
+			$size = @getimagesize($fullpath);
+			if( $size && $size[0] > 0 ) {
+				$text .= ' width="'.$size[0].'"';
+			}
+		}
 
-      if( !empty($params['height'] ) ) {
-          $text .= ' height="'.$params['height'].'"';
-      } elseif ($size[1] > 0) {
-          $text .= ' height="'.$size[1].'"';
-      }
+		if( !empty($params['height']) ) {
+			$text .= ' height="'.trim($params['height'],' "\'').'"';
+		} else {
+			if( !isset($size) ) $size = @getimagesize($fullpath);
+			if( $size && $size[1] > 0 ) {
+				$text .= ' height="'.$size[1].'"';
+			}
+		}
 
-      if( !empty($params['alt'] ) ) {
-          $alt = $params['alt'];
-      } else {
-          $alt = '['.$params['src'].']';
-      }
+		if( !empty($params['alt']) ) {
+			$alt = trim($params['alt'],' "\'');
+		} else {
+			$alt = basename($fullpath);
+		}
+		$text .= ' alt="'.$alt.'"';
 
-      $text .= ' alt="'.$alt.'"';
-      if( !empty($params['title'] ) )	{
-          $text .= ' title="'.$params['title'].'"';
-      } else {
-          $text .= ' title="'.$alt.'"';
-      }
+		if( !empty($params['title']) ) {
+			$text .= ' title="'.$params['title'].'"';
+		} else {
+			$text .= ' title="'.$alt.'"';
+		}
 
-      if( !empty($params['class'] ) )	$text .= ' class="'.$params['class'].'"';
-      if( !empty($params['addtext'] ) ) $text .= ' ' . $params['addtext'];
-      $text .= $imgend;
-  } else {
-      $text = '<!-- empty results from image plugin -->';
-  }
+		if( !empty($params['class']) ) $text .= ' class="'.$params['class'].'"';
+		if( !empty($params['addtext']) ) $text .= ' ' . $params['addtext'];
+		$text .= '>';
+	} else {
+		$text = '<!-- empty results from image plugin -->';
+	}
 
-  if( isset($params['assign']) )	{
-      $smarty->assign(trim($params['assign']),$text);
-      return '';
-  }
-  return $text;
+	if( !empty($params['assign']) ) {
+		$template->assign(trim($params['assign']),$text);
+		return '';
+	}
+	return $text;
 }
 
 
@@ -78,6 +80,7 @@ function smarty_cms_about_function_image()
 		<li>Added alt param and removed the &lt;/img&gt;</li>
 		<li>Added default width, height and alt <small>(contributed by Walter Wlodarski)</small></li>
 		<li>Deprecated 2017-06-10, instead use page_image tag</li>
+		<li>Undeprecated 2025-01-20, the page_image tag does not sufficiently replace image</li>
 	</ul>
 <?php
 }
