4df791c96b28c293056c0c9425664f70379dbd23
tom2tom <tpgww@onepost.net>
2026-04-26 17:34:58 +1000

    add class namespace

--- modules/CMSContentManager/lib/class.Utils.php
+++ modules/CMSContentManager/lib/class.Utils.php
@@ -1,37 +1,34 @@
 <?php
 #BEGIN_LICENSE
 #-------------------------------------------------------------------------
-# Module CMSContentManager class CmsContentManagerUtils
+# Module CMSContentManager class Utils
 # (c) 2013 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
-#
 #-------------------------------------------------------------------------
-#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# However, as a special exception to the GPL, this software is distributed
-# as an addon module to CMS Made Simple.  You may not use this software
-# in any Non GPL version of CMS Made simple, or in any version of CMS
-# Made simple that does not indicate clearly and obviously in its admin
-# section that the site was built with CMS Made simple.
-#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
 #
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, read the license online at:
+# https://www.gnu.org/licenses/#LicenseURLs
 #-------------------------------------------------------------------------
 #END_LICENSE
 
-/**
- * @package CMS
- */
+namespace CMSContentManager;
+
+use cms_userprefs;
+use cms_utils;
+use CmsDataNotFoundException;
+use CmsLayoutCollection;
+use CmsLayoutTemplate;
+use CmsLayoutTemplateType;
+use function get_userid;
 
 /**
  * Utility methods for the CMSContentManager module.
@@ -43,7 +40,7 @@
  * @ignore
  * @author Robert Campbell
  */
-final class CmsContentManagerUtils
+final class Utils
 {
 	private function __construct() {}
 
@@ -53,7 +50,7 @@ final class CmsContentManagerUtils
 			$tpl = CmsLayoutTemplate::load_dflt_by_type(CmsLayoutTemplateType::CORE.'::page');
 			$tpl_id = $tpl->get_id();
 		}
-		catch( \CmsDataNotFoundException $e ) {
+		catch( CmsDataNotFoundException $e ) {
 			$type = CmsLayoutTemplateType::load(CmsLayoutTemplateType::CORE.'::page');
 			$list = CmsLayoutTemplate::load_all_by_type($type);
 			$tpl = $list[0];
@@ -101,8 +98,5 @@ final class CmsContentManagerUtils
 		}
 		return $pref;
 	}
-} // end of class
-#
-# EOF
-#
+}
 ?>
