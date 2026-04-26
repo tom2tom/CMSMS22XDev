23c504758050aa63316fe39d43dc3665f933a501
tom2tom <tpgww@onepost.net>
2026-04-26 17:39:49 +1000

    typo

--- modules/CMSContentManager/lib/class.EditContentAssistant.php
+++ modules/CMSContentManager/lib/class.EditContentAssistant.php
@@ -3,29 +3,21 @@
 #-------------------------------------------------------------------------
 # Module CMSContentManager class EditContentAssistant
 # (c) 2013 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
-#
 #-------------------------------------------------------------------------
-#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# However, as a special exception to the GPL, this software is distributed
-# as an addon module to CMS Made Simple.  You may not use this software
-# in any Non GPL version of CMS Made simple, or in any version of CMS
-# Made simple that does not indicate clearly and obviously in its admin
-# section that the site was built with CMS Made simple.
-#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
-#
+# Or read it online: https://www.gnu.org/licenses/licenses.html#GPL
 #-------------------------------------------------------------------------
 #END_LICENSE
 
@@ -55,8 +47,4 @@ abstract class EditContentAssistant implements ContentAssistant
    */
   abstract public function getExtraCode();
 }
-
-#
-# EOF
-#
 ?>
