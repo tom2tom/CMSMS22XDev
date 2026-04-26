3c2ca77c891f0e0d90fb3ad2dfe06f8a4d89b0df
tom2tom <tpgww@onepost.net>
2026-04-26 16:26:07 +1000

    typo

--- modules/CMSContentManager/action.admin_editcontent.php
+++ modules/CMSContentManager/action.admin_editcontent.php
@@ -16,7 +16,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, read the license online at:
-# http://www.gnu.org/licenses/#LicenseURLs
+# https://www.gnu.org/licenses/#LicenseURLs
 #-------------------------------------------------------------------------
 #END_LICENSE
 if( !isset($gCms) ) exit;
