208c169e01a77f43ea117c7e9ac8adba97f57897
tom2tom <tpgww@onepost.net>
2026-04-26 16:11:52 +1000

    add license use namespaced class

--- modules/ModuleManager/function.admin_installed.php
+++ modules/ModuleManager/function.admin_installed.php
@@ -1,9 +1,15 @@
 <?php
+/*
+CMSMS ModuleManger module tab-populator: admin_installed
+(C) 2008 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
+The license at the top of file ModuleManager.module.php applies to this file.
+*/
+
 if( !isset($gCms) ) exit;
 if( !$this->CheckPermission('Modify Modules') ) return;
 
 try {
-    $allmoduleinfo = ModuleManagerModuleInfo::get_all_module_info($connection_ok);
+    $allmoduleinfo = ModuleManager\ModuleInfo::get_all_module_info($connection_ok);
     uksort($allmoduleinfo,'strnatcasecmp');
     $tpl->assign('module_info',$allmoduleinfo);
 }
@@ -14,3 +20,4 @@ catch( Exception $e ) {
 }
 $tpl->assign('allow_export',(!empty($config['developer_mode']))?1:0);
 $tpl->assign('allow_modman_uninstall',$this->GetPreference('allowuninstall',0));
+?>
