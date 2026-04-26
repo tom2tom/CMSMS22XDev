aecc0ee7cc6e995f46c7b9b9b25dcd53f5e02fd3
tom2tom <tpgww@onepost.net>
2026-04-26 16:08:31 +1000

    extends cms_http_request class

--- modules/ModuleManager/lib/class.cached_http_request.php
+++ modules/ModuleManager/lib/class.cached_http_request.php
@@ -1,112 +1,134 @@
 <?php
 #BEGIN_LICENSE
 #-------------------------------------------------------------------------
-# Module ModuleManager class modmgr_cached_request
+# CMSMS module ModuleManager class cached_http_request
 # (c) 2011 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
-#
 #-------------------------------------------------------------------------
-#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# However, as a special exception to the GPL, this software is distributed
-# as an addon module to CMS Made Simple.  You may not use this software
-# in any Non GPL version of CMS Made simple, or in any version of CMS
-# Made simple that does not indicate clearly and obviously in its admin
-# section that the site was built with CMS Made simple.
-#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
 #
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, read the license online at:
+# https://www.gnu.org/licenses/#LicenseURLs
 #-------------------------------------------------------------------------
 #END_LICENSE
 
-final class modmgr_cached_request
-{
-  private $_status;
-  private $_result;
-  private $_timeout;
-  private $_signature;
+namespace ModuleManager;
 
-  private function _getCacheFile()
+use cms_http_request;
+use cms_siteprefs;
+use cms_utils;
+use Exception;
+use const PUBLIC_CACHE_LOCATION;
+use function audit;
+use function cmsms;
+
+/**
+ * This class extends core class cms_http_request.
+ * The result from executing a request via that class is cached, and
+ * subsequent identical requests use that cached result instead of doing
+ * a fresh request to the target.
+ * @since 2.1.12 Formerly modmgr_cached_request class
+ */
+final class cached_http_request extends cms_http_request
+{
+  private $signature;
+  private $age; // cache-file timeout (minutes)
+
+  public function __construct($params = [])
   {
-    if( $this->_signature ) {
-      $fn = TMP_CACHE_LOCATION.'/modmgr_'.$this->_signature.'.dat';
-      return $fn;
+    if( $params ) {
+      if( isset($params['age']) ) {
+        $this->age = max(1,(int)$params['age']);
+        unset($params['age']);
+      }
     }
+    parent::__construct($params);
   }
 
-  public function execute($target = '',$data = array(), $age = '')
+  /**
+   * @since 2.1.12 formerly execute()
+   * @param string $target Default ''
+   * @param array $data default []
+   * @return void
+   */
+  public function send($target = '',$data = [])
   {
     // build a signature
-    $this->_signature = md5(serialize(array($target,$data)));
-    $fn = $this->_getCacheFile();
-    if( !$fn ) return;
+    $this->signature = md5(serialize([$target,$data]));
+    $fn = $this->getCacheFile();
+    if( !$fn ) return '';
 
-    if( !$age ) $age = cms_siteprefs::get('browser_cache_expiry',60);
-    if( $age ) $age = max(1,(int)$age);
+    if( empty($this->age) ) {
+      $age = cms_siteprefs::get('browser_cache_expiry',60);
+    }
+    else {
+      $age = $this->age;
+    }
+    $age = max(1,(int)$age);
     $atime = time() - ($age * 60);
     $mod = cms_utils::get_module('ModuleManager');
     $config = cmsms()->GetConfig();
     // check for the cached file
-    if( (!empty($config['developer_mode']) && $mod->GetPreference('disable_caching',0)) ||
-        !file_exists($fn) || filemtime($fn) <= $atime ) {
-        // execute the request
-        $req = new cms_http_request();
-        if( $this->_timeout ) $req->setTimeout($this->_timeout);
-        $res = $req->execute($target,'','POST',$data);
-        $this->_status = $req->getStatus();
-        $this->_result = $req->getResult(); // aka $res
+    if( (!file_exists($fn) || filemtime($fn) <= $atime) ||
+        (!empty($config['developer_mode']) && $mod->GetPreference('disable_caching',false)) ) {
+      $this->setMethod('POST');
+      parent::send($target,$data);
 
-        if( file_exists($fn) ) @unlink($fn);
-        if( $this->_status == 200 ) {
-            // create a cache file
-            $fh = fopen($fn,'w');
-            fwrite($fh,serialize(array($this->_status,$this->_result)));
-            fclose($fh);
-        } else {
-            audit('',$mod->GetName(),'Request to module repository resulted in status '.$this->_status);
-        }
+      if( file_exists($fn) ) @unlink($fn);
+      if( $this->status == 200 ) {
+        // record a cache file
+        $fh = fopen($fn,'wb');
+        fwrite($fh,serialize(array($this->status,$this->result)));
+        fclose($fh);
+      } else {
+        audit('',$mod->GetName(),'Request to module repository resulted in status '.$this->status);
+      }
     }
     else {
-        // get data from the cache.
-        $data = unserialize(file_get_contents($fn));
-        $this->_status = $data[0];
-        $this->_result = $data[1];
+      // get cached request-data
+      $data = unserialize(file_get_contents($fn),['allowed_classes'=>[]]);
+      if( $data ) {
+        $this->status = $data[0];
+        $this->result = $data[1];
+      }
+      else {
+        throw new Exception('Failed to retrieve cached request-data');
+      }
     }
+    return $this->result;
   }
 
-  public function setTimeout($val)
+  public function setTimeout($seconds)
   {
-    $this->_timeout = max(1,min(1000,(int)$val));
+    $this->timeout = max(1,min(1000,(int)$seconds));
   }
 
-  public function getStatus()
+  public function setAge($minutes)
   {
-    return $this->_status;
-  }
-
-  public function getResult()
-  {
-    return $this->_result;
+    $this->age = max(1,(int)$minutes);
   }
 
   public function clearCache()
   {
-    $fn = $this->_getCacheFile();
-    if( $fn ) @unlink($fn);
+    $fn = $this->getCacheFile();
+    if( $fn && is_file($fn) ) { @unlink($fn); }
   }
-} // end of class.
 
-#
-# EOF
-#
+  private function getCacheFile()
+  {
+    if( $this->signature ) {
+      return PUBLIC_CACHE_LOCATION.DIRECTORY_SEPARATOR.'modmgr_'.$this->signature.'.dat';
+    }
+    return '';
+  }
+}
+
 ?>
